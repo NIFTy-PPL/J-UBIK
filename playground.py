d01be5c2197fa5a216e8361ed8167a493194bbd2
Vincent Eberle <VE@Vincents-MBP-2.fritz.box>
2021-03-05 22:09:25 +0100

    work

--- playground.py
+++ playground.py
@@ -7,7 +7,7 @@ fov = 4.
 position_space = ift.RGSpace([npix_s, npix_s], distances=[ 2.*fov/npix_s])
 zp_position_space = ift.RGSpace([2.*npix_s, 2. * npix_s], distances=[ 2.*fov/npix_s])
 
-info = np.load('262_observation.npy', allow_pickle= True).item()
+info = np.load('5_3_observation.npy', allow_pickle= True).item()
 data = info['data'].val[:,:,1]
 data = ift.Field.from_raw(position_space, data)
 
@@ -17,60 +17,78 @@ exp_field = ift.Field.from_raw(position_space, exp_field)
 plot= ift.Plot()
 exp_norm = (data / exp_field).mean()
 normed_exp_field =  exp_field * exp_norm.val
-plot.add(ift.log(normed_exp_field))
-plot.add(data)
-plot.output()
-exit()
+#FIXME MASK with 0 and NAN entrys
 
+points = ift.InverseGammaOperator(position_space, 2.5, 1.5).ducktape('points')
 args = {
-        'offset_mean': 0,
-        'offset_std': (1e-3, 1e-6),
+        'offset_mean': 2,
+        'offset_std': (1e-1, 1e-2),
 
         # Amplitude of field fluctuations
-        'fluctuations': (2., 1.),  # 1.0, 1e-2
+        'fluctuations': (0.5, 0.5),  # 1.0, 1e-2
 
         # Exponent of power law power spectrum component
-        'loglogavgslope': (-4., 1),  # -6.0, 1
+        'loglogavgslope': (-2., 1),  # -6.0, 1
 
         # Amplitude of integrated Wiener process power spectrum component
-        'flexibility': (5, 2.),  # 2.0, 1.0
+        'flexibility': (3, 2.),  # 2.0, 1.0
 
         # How ragged the integrated Wiener process component is
-        'asperity': (0.5, 0.5)  # 0.1, 0.5
+        'asperity': (0.2, 0.5)  # 0.1, 0.5
         }
 
-diffuse = ift.SimpleCorrelatedField(zp_position_space, **args).real
+diffuse = ift.SimpleCorrelatedField(position_space, **args).real
 diffuse = ift.exp(diffuse)
-zp_signal = ift.FieldZeroPadder(position_space, zp_position_space.shape)
+# zp_signal = ift.FieldZeroPadder(position_space, zp_position_space.shape)
 
-signal = diffuse
+signal = diffuse + points
 Mask = ift.DiagonalOperator(normed_exp_field)
-FFT = ift.FFTOperator(zp_position_space)
+# FFT = ift.FFTOperator(zp_position_space)
 
-kernel = np.zeros(zp_position_space.shape)
-kernel[0,0] = 1024
-kernel = ift.Field.from_raw(zp_position_space, kernel)
-psf = FFT(kernel)
-psf = ift.DiagonalOperator(psf)
+# kernel = np.zeros(zp_position_space.shape)
+# kernel[0,0] = 1024
+# kernel = ift.Field.from_raw(zp_position_space, kernel)
+# psf = FFT(kernel)
+# psf = ift.DiagonalOperator(psf)
 
-conv = FFT.inverse @ psf @ FFT @ diffuse
-conv = conv.real
-signal_response = Mask@ zp_signal.adjoint @ conv
-
-
-ic_newton = ift.DeltaEnergyController(
-name='Newton', iteration_limit=50, tol_rel_deltaE=1e-8)
-minimizer = ift.NewtonCG(ic_newton)
+# conv = FFT.inverse @ psf @ FFT @ diffuse
+# conv = conv.real
+# signal_response = Mask@ zp_signal.adjoint @ conv
+signal_response = Mask@signal
+ic_newton = ift.AbsDeltaEnergyController(name='Newton', deltaE=0.5, iteration_limit=10, convergence_level=3)
+ic_sampling = ift.AbsDeltaEnergyController(deltaE=0.05, iteration_limit = 200)
 likelihood = ift.PoissonianEnergy(data) @ signal_response
-H = ift.StandardHamiltonian(likelihood)
+
+minimizer = ift.NewtonCG(ic_newton)
+
+H = ift.StandardHamiltonian(likelihood, ic_sampling)
+initial_position = 0.1*ift.from_random(H.domain)
+pos = initial_position
 
 
-initial_position = ift.from_random(H.domain)
-H = ift.EnergyAdapter(initial_position, H, want_metric=True)
-H, _ = minimizer(H)
+if False:
+    H=ift.EnergyAdapter(pos, H, want_metric=True)
+    H,_ = minimizer(H)
+    plt = ift.Plot()
+    plt.add(data)
+    plt.add(signal_response.force(H.position))
+    plt.add(diffuse.force(H.position))
+    plt.output()
+else:
+    for ii in range(10):
+        KL = ift.MetricGaussianKL.make(pos, H, 5, True)
+        KL, _ = minimizer(KL)
+        pos = KL.position
+        samples = list(KL.samples)
+        ift.extra.minisanity(data, lambda x: ift.makeOp(signal_response(x)), signal_response, pos, samples)
 
-plt = ift.Plot()
-plt.add((data), vmin= 0, title = 'data')
-plt.add((zp_signal.adjoint(diffuse.force(H.position))), title = 'signal')
-plt.add((signal_response.force(H.position)), title= 'signalresponse')
-plt.output()
+        plt = ift.Plot()
+        sc = ift.StatCalculator()
+        for foo in samples:
+            sc.add((signal.force(foo.unite(KL.position))))
+        plt.add(ift.log(sc.mean), title="Reconstructed Signal")
+        plt.add(sc.var.sqrt(), title = "Relative Uncertainty")
+        plt.add(ift.log((signal_response.force(KL.position))), title= 'signalresponse')
+        plt.add(ift.log((data)), vmin= 0, title = 'data')
+        plt.add((ift.abs(signal_response.force(KL.position)-data)), title = "Residual")
+        plt.output(name= f'rec_{ii}.png')
--- reconstruction.py
+++ reconstruction.py
@@ -2,7 +2,7 @@ import nifty7 as ift
 import numpy as np
 import sys
 
-from obs.obs11713 import obs11713
+from obs.obs4952 import obs4952
 from lib.observation import ChandraObservationInformation
 from lib.output import plot_slices
 #NOTE this src clashes with nifty7 somehow
@@ -18,7 +18,7 @@ outroot = sys.argv[1]
 data_domain = ift.DomainTuple.make([ift.RGSpace((npix_s, npix_s), distances=2.*fov/npix_s),\
                                     ift.RGSpace((npix_e,), distances=np.log(elim[1]/elim[0])/npix_e)])
 
-info     = ChandraObservationInformation(obs11713, npix_s, npix_e, fov, elim, center=None)
+info     = ChandraObservationInformation(obs4952, npix_s, npix_e, fov, elim, center=None)
 
 # retrive data
 data     = info.get_data('./data.fits')
@@ -31,10 +31,10 @@ exposure = ift.makeField(data_domain, exposure)
 plot_slices(exposure, outroot+'_exposure.png', logscale=True)
 
 # simulate the PSF for one location
-psf_sim  = info.get_psf_fromsim( (info.obsInfo['aim_ra'], info.obsInfo['aim_dec']), 'ACIS-I', './psf')
-psf_sim  = ift.makeField(data_domain, psf_sim)
-plot_slices(psf_sim, outroot+'_psfSIM.png', logscale=True)
+# psf_sim  = info.get_psf_fromsim( (info.obsInfo['aim_ra'], info.obsInfo['aim_dec']), 'ACIS-I', './psf')
+# psf_sim  = ift.makeField(data_domain, psf_sim)
+# plot_slices(psf_sim, outroot+'_psfSIM.png', logscale=True)
 
-np.save(outroot+'observation.npy', {'data':data, 'exposure':exposure, 'psf_sim':psf_sim})
+np.save(outroot+'observation.npy', {'data':data, 'exposure':exposure})#, 'psf_sim':psf_sim})
 
 exit()
