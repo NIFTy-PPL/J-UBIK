c11482d22a0376b24d93b17ff4cc5c59b06f3c84
veberle <veberle@mpa-garching.mpg.de>
2021-07-20 09:29:03 +0200

    comments

--- fit_mf.py
+++ fit_mf.py
@@ -11,24 +11,23 @@ ift.fft.set_nthreads(2)
 npix_s = 1024      # number of spacial bins per axis
 npix_e = 4
 fov = 4.
-elim   = (2., 10.) # energy range in keV #TODO Lower limit? sollte das nicht bis 0.1 keV runter gehen=
+elim   = (2., 10.) # energy range in keV #FIXME check limits
 
-position_space = ift.RGSpace([npix_s, npix_s], distances=[ 2.*fov/npix_s])
+position_space = ift.RGSpace([npix_s, npix_s], distances=[2.*fov/npix_s])
 e_space = ift.RGSpace(npix_e, distances=np.log(elim[1]/elim[0])/npix_e)
 dom = ift.makeDomain([position_space, e_space])
-
-zp_position_space = ift.RGSpace([2.*npix_s, 2. * npix_s], distances=[ 2.*fov/npix_s])
+zp_position_space = ift.RGSpace([2.*npix_s, 2. * npix_s], distances=[ 2.*fov/npix_s]) #FIXME less zeropadding enough?
 
 info = np.load('14_6_0_observation.npy', allow_pickle= True).item()
 psf_file = np.load('psf_ob0.npy', allow_pickle = True).item()
-
-plot_slices(psf_file, 'raw.png', True)
-
 psf_arr = psf_file.val
 max_idx = np.unravel_index(np.argmax(psf_arr[:,:,0]), psf_arr[:,:,0].shape)
+
+#FIXME why are the maxima of PSF not on one spot? Statistics?
 # for i in range(4):
 # # THEYARE NOT EQUAL
 #     print(np.argmax(psf_arr[:,:,i]))
+
 psf_arr = np.roll(psf_arr, -max_idx[0], axis=0)
 psf_arr = np.roll(psf_arr, -max_idx[1], axis=1)
 psf_field = ift.Field.from_raw(dom, psf_arr)
@@ -42,9 +41,11 @@ normed_exposure = get_normed_exposure(exp, data)
 normed_exposure = ift.makeOp(normed_exposure)
 mask = get_mask_operator(exp)
 
+#FIXME Need for energy correlation
 # points = ift.InverseGammaOperator(zp_position_space, alpha=1.0, q=1e-4).ducktape('points')
 # points_energy = ift.Simple
 
+#FIXME put this into a config file for later runs
 priors_sp_diffuse = {
         # Amplitude of field fluctuations
         'fluctuations': (0.5, 0.5),  # 1.0, 1e-2
@@ -94,7 +95,7 @@ priors_e_agn = {
         'fluctuations': (1, 0.5),  # 1.0, 1e-2
 
         # Exponent of power law power spectrum component
-        'loglogavgslope': (-1, 0.5),  # -6.0, 1
+        'loglogavgslope': (-1.5, 0.5),  # -6.0, 1
 
         # Amplitude of integrated Wiener process power spectrum component
         'flexibility': (0.5, 0.05),  # 2.0, 1.0
@@ -131,8 +132,7 @@ signal_response = mask @ normed_exposure @ conv
 ic_newton = ift.AbsDeltaEnergyController(name='Newton', deltaE=0.5, convergence_level=3, iteration_limit=4)
 ic_sampling = ift.AbsDeltaEnergyController(name='Samplig(lin)', deltaE=0.05, iteration_limit = 100)
 
-masked_data = mask(data)
-#is there data where there should#nt?
+masked_data = mask(data) #FIXME this is kind of broken
 likelihood = ift.PoissonianEnergy(masked_data) @ signal_response
 minimizer = ift.NewtonCG(ic_newton)
 H = ift.StandardHamiltonian(likelihood, ic_sampling)
@@ -196,7 +196,7 @@ else:
         }
         #TODO add samples
         np.save('varinf_reconstruction.npy', dct)
-
+        np.save('lat_mean.npy', pos)
         # for oo, nn in [(extended, "extended"), (diffuse, "diffuse"), (points, "points")]:
         #     samps = []
         #     for foo in samples:
