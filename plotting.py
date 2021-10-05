93322929b08b132cc0a970626d58e35fc3bd32c8
veberle <veberle@mpa-garching.mpg.de>
2021-10-05 15:07:46 +0200

    clean and format

--- plotting.py
+++ plotting.py
@@ -4,12 +4,8 @@ from lib.output import plot_result
 
 
 if True:
-        dct = np.load('varinf_reconstruction.npy', allow_pickle = True).item()
-        for  name in dct:
-                plot_result(dct[name],f'var_{name}.png')
-        residual = ift.abs(dct['signal_response']- dct['data'])
-        plot_result(residual, 'residual.png')
-# else:
-#         dct = np.load('map_reconstruction.npy', allow_pickle = True).item()
-#         for  name in dct:
-#                 plot_result(dct[name],f'map_{name}.png')
+    dct = np.load("varinf_reconstruction.npy", allow_pickle=True).item()
+    for name in dct:
+        plot_result(dct[name], f"var_{name}.png")
+    residual = ift.abs(dct["signal_response"] - dct["data"])
+    plot_result(residual, "residual.png")
