24d6f050a4eeb4041e5e6e24d97d66b02bbe6d70
veberle <veberle@mpa-garching.mpg.de>
2021-12-03 11:07:26 +0100

    data read in

--- obs/obs4952.py
+++ obs/obs4952.py
@@ -2,10 +2,10 @@ obs4952 = {
     # 1. specify input data
     #######################
     "obsID": 4952,
-    "data_location": "data/4952/",
-    "event_file": "primary/acisf04952N004_evt2.fits",
-    "aspect_sol": "primary/pcadf04952_000N001_asol1.fits",
-    "bpix_file": "primary/acisf04952_000N004_bpix1.fits",
-    "mask_file": "secondary/acisf04952_000N004_msk1.fits",
+    "data_location": "data/4952/repro/",
+    "event_file": "acisf04952_repro_evt2.fits",
+    "aspect_sol": "pcadf04952_000N001_asol1.fits",
+    "bpix_file": "acisf04952_repro_bpix1.fits",
+    "mask_file": "acisf04952_000N004_msk1.fits",
     "instrument": "ACIS-S",
 }
