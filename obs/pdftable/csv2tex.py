227684a83bfe028c1aa52a952540a7d5cc9e1c26
veberle <veberle@mpa-garching.mpg.de>
2022-01-24 15:06:24 +0100

    remanufacture

--- obs/pdftable/csv2tex.py
+++ obs/pdftable/csv2tex.py
@@ -8,4 +8,4 @@ with open("pdftable/keys.yaml", 'r') as cfg_file:
     cfg = yaml.safe_load(cfg_file)
 
 p = pd.concat([df[key] for key in cfg['keys']], axis=1) 
-p.to_latex('pdftable/tab.tex', index=False, longtable=True)
+p.to_latex('pdftable/tab.tex', index=False)
