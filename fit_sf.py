05554375a26558c2480937c0d374ab595241ed8d
Vincent Eberle <veberle@mpa-garching.mpg.de>
2022-02-11 10:29:15 +0100

    doing everything twice

--- fit_sf.py
+++ fit_sf.py
@@ -15,24 +15,50 @@ energy_bin = 0
 position_space = ift.RGSpace([npix_s, npix_s], distances=[2.0 * fov / npix_s])
 
 info = np.load(cfg['dataset']['observation'], allow_pickle=True).item()
-print(info)
 psf_file = np.load("psf_obs4952.npy", allow_pickle=True).item()
 
+### PSF1
 psf_arr = psf_file.val[:, :, energy_bin]
 psf_arr = np.roll(psf_arr, -np.argmax(psf_arr))
 psf_field = ift.Field.from_raw(position_space, psf_arr)
 norm = ift.ScalingOperator(position_space, psf_field.integrate().val ** -1)
 psf_norm = norm(psf_field)
+
+### PSF2
+psf_arr = psf_file.val[:, :, energy_bin]
+psf_arr = np.roll(psf_arr, -np.argmax(psf_arr))
+psf_field = ift.Field.from_raw(position_space, psf_arr)
+norm = ift.ScalingOperator(position_space, psf_field.integrate().val ** -1)
+psf_norm = norm(psf_field)
+
+#Data 1
 data = info["data"].val[:, :, energy_bin]
 data_field = ift.Field.from_raw(position_space, data)
 
+
+#Data 2
+data = info["data"].val[:, :, energy_bin]
+data_field = ift.Field.from_raw(position_space, data)
+
+#Exp 1
 exp = info["exposure"].val[:, :, energy_bin]
 exp_field = ift.Field.from_raw(position_space, exp)
-normed_exposure = get_normed_exposure(exp_field, data_field)
-normed_exposure = ift.makeOp(normed_exposure)
+# normed_exposure = get_normed_exposure(exp_field, data_field)
+normed_exposure = ift.makeOp(exp_field)
 
+#Exp 1
+exp = info["exposure"].val[:, :, energy_bin]
+exp_field = ift.Field.from_raw(position_space, exp)
+# normed_exposure = get_normed_exposure(exp_field, data_field)
+normed_exposure = ift.makeOp(exp_field)
+
+#Mask 1
 mask = get_mask_operator(exp_field)
 
+#Mask 2
+mask = get_mask_operator(exp_field)
+
+#Model
 diffuse = ift.SimpleCorrelatedField(position_space, **cfg['priors_diffuse'])
 pspec = diffuse.power_spectrum
 diffuse = diffuse.exp()
@@ -41,12 +67,21 @@ points = points.ducktape("points")
 signal = points + diffuse
 signal = signal.real
 
+#Likelihood 1
 transpose = Transposer(signal.target)
 psf = psf_norm
 convolved = convolve_field_operator(psf, signal)
 conv = convolved
-
 signal_response = mask @ normed_exposure @ conv
+
+
+#Likelihood 2
+transpose = Transposer(signal.target)
+psf = psf_norm
+convolved = convolve_field_operator(psf, signal)
+conv = convolved
+signal_response = mask @ normed_exposure @ conv
+
 
 ic_newton = ift.AbsDeltaEnergyController(**cfg['ic_newton'])
 ic_sampling = ift.AbsDeltaEnergyController(**cfg['ic_sampling'])
@@ -58,12 +93,6 @@ H = ift.StandardHamiltonian(likelihood, ic_sampling)
 
 nl_sampling_minimizer = None
 pos = 0.1 * ift.from_random(signal.domain)
-
-if False:
-    pos = ift.ResidualSampleList.load_mean("new_rec/pickle/last")
-    rstate = open("new_rec/pickle/nifty_random_state_last", "rb").read()
-    ift.random.setState(rstate)
-    print("loaded")
 
 
 def callback(samples):
