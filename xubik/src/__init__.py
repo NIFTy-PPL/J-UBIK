1ad66af39b005c22d0ef0c8ff742f5dc40feb44c
Vincent Eberle <veberle@mpa-garching.mpg.de>
2022-02-24 11:35:12 +0100

    more restructuring

--- xubik/src/__init__.py
+++ xubik/src/__init__.py
@@ -1,5 +1,7 @@
 from .lib.utils import *
+from .lib.plot import *
 
-from .operators.observation import *
+from .operators.observation_operator import *
 from .operators.convolution_operators import *
+from .operators.bilinear_interpolation import *
 from .operators.zero_padder import *
