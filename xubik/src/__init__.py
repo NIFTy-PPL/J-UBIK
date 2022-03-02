aa083c2c58f9caa16fc8ee709e9cefaa63f93267
Vincent Eberle <veberle@mpa-garching.mpg.de>
2022-03-02 16:12:02 +0100

    new stuff to init

--- xubik/src/__init__.py
+++ xubik/src/__init__.py
@@ -1,7 +1,7 @@
 from .library.utils import (get_mask_operator, prior_sample_plotter, get_norm, get_normed_exposure,
                             get_data_domain, get_cfg, convolve_operators, convolve_field_operator,
                             Transposer, coord_center, get_radec_from_xy, get_psfpatches)
-from .library.plot import plot_slices, plot_result
+from .library.plot import plot_slices, plot_result, plot_single_psf, plot_psfset
 from .library import mpi
 
 from .operators.observation_operator import ChandraObservationInformation
