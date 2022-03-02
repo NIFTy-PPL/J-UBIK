8727abf8ea2eb4fb9e3d493ad7c8d2d89d7ea71a
Vincent Eberle <veberle@mpa-garching.mpg.de>
2022-03-02 16:11:42 +0100

    this is more a script than part of xubik

--- getpsf_trainset.py
+++ getpsf_trainset.py
@@ -6,16 +6,18 @@ import ciao_contrib.runtool as rt
 import nifty8 as ift
 import xubik0 as xu
 
-########## RECONSTRUCTION PARAMETERS ##########
-npix_s = 1024  # number of spacial bins per axis
-npix_e = 4  # number of log-energy bins
-fov = 4.0  # FOV in arcmin
-elim = (2.0, 10.0)  # energy range in keV
-################################################
-
 obs_info = xu.get_cfg("obs/obs.yaml")
 obs4952 = obs_info["obs4952"]
 obs11713 = obs_info["obs11713"]
+
+img_cfg = xu.get_cfg("config.yaml")
+grid = img_cfg["grid"]
+
+npix_s = grid["npix_s"] # number of spacial bins per axis
+npix_e = grid["npix_e"]  # number of log-energy bins
+fov = grid["fov"]  # FOV in arcmin
+elim = grid["elim"]  # energy range in keV
+
 outroot = "patches_"
 data_domain = ift.DomainTuple.make(
     [
@@ -31,7 +33,7 @@ ref = (
     info.obsInfo["aim_dec"],
 )  # Center of the FOV in sky coords
 
-if True:
+if False:
     info = xu.ChandraObservationInformation(
         obs11713,
         npix_s,
@@ -42,5 +44,7 @@ if True:
     )
 n = 8
 ebin = 0
-psf_sim = xu.get_psfpatches(info, n, npix_s, ebin, fov)
-np.save(outroot + "psf.npy", {"psf_sim": psf_sim})
+psf_sim, sources, pos, coords = xu.get_psfpatches(info, n, npix_s, ebin, fov)
+np.save(outroot + "psf.npy", {"psf_sim": psf_sim, "sources": sources})
+
+xu.plot_psfset("patches_psf.npy",1024, 8, False)
