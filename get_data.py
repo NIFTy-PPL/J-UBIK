b87529d61e4a113abbff1bdb553cb8f0ebe387c0
Vincent Eberle <veberle@mpa-garching.mpg.de>
2022-03-29 12:21:30 +0200

    convert obsnr to str

--- get_data.py
+++ get_data.py
@@ -12,11 +12,11 @@ data_domain = xu.get_data_domain(grid)
 center = None
 for obsnr in obslist:
     outfile = outroot + f"_{obsnr}_" + "observation.npy"
-    info = xu.ChandraObservationInformation(obs_info["obs" + obsnr], **grid, center=center)
     data = info.get_data(f"./data_{obsnr}.fits")
     data = ift.makeField(data_domain, data)
     #FIXME info.get_data could also directly return a field, ChandraObservationInformation probably builds the same domain within
     xu.plot_slices(data, outroot + f"_data_{obsnr}.png", logscale=True)
+    info = xu.ChandraObservationInformation(obs_info["obs" + str(obsnr)], **grid, center=center)
 
     # compute the exposure map
     exposure = info.get_exposure(f"./exposure_{obsnr}")
@@ -24,7 +24,6 @@ for obsnr in obslist:
     xu.plot_slices(exposure, outroot + f"_exposure_{obsnr}.png", logscale=True)
 
     # compute the point spread function
-    print(cfg["psf_sim"]['num_rays'])
     psf_sim = info.get_psf_fromsim(
         (info.obsInfo["aim_ra"], info.obsInfo["aim_dec"]), "./psf", num_rays=cfg["psf_sim"]['num_rays'])
     psf_sim = ift.makeField(data_domain, psf_sim)
