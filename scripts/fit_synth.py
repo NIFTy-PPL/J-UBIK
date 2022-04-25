edd9ac6818edd2fdad8eee7a837d764667b2b272
Vincent Eberle <veberle@mpa-garching.mpg.de>
2022-04-25 17:26:23 +0200

    WIP: fit_synth

--- scripts/fit_synth.py
+++ scripts/fit_synth.py
@@ -14,15 +14,20 @@ fov = 4.0
 position_space = ift.makeDomain(ift.RGSpace([npix_s, npix_s], distances=[2.0 * fov / npix_s]))
 
 points = ift.InverseGammaOperator(position_space, **cfg['points'])
-signal = points.ducktape("points").real.ducktape_left("signal")
+signal = points.ducktape("points").real
+signal_dl = signal.ducktape_left("signal")
 # PSF
 if inhomogen:
     psf_file = np.load("../data/npdata/psf_patches/4952_patches_v2.npy", allow_pickle=True).item()["psf_sim"]
+    psfs = psf_file #TODO NORMALIZE
     psfs = []
     for p in psf_file:
-        psfs.append(p.val)
+        norm_val = p.integrate().val**-1
+        norm = ift.ScalingOperator(p.domain, norm_val)
+        psf_norm = norm(p)
+        psfs.append(psf_norm.val)
     psfs = np.array(psfs, dtype="float64")
-    conv = xu.OverlapAddConvolver(position_space, psfs, 64, 128)
+    conv = xu.OverlapAddConvolver(position_space, psfs, 64, 64)
     cut = xu.MarginZeroPadder(position_space, ((conv.target.shape[0] - position_space.shape[0])//2), space=0).adjoint
     response = cut @ conv
     response = response.ducktape("signal")
@@ -38,31 +43,36 @@ else:
     response = xu.convolve_field_operator(psf, signal_prox)
 
 # Data
-data = np.load("../data/npdata/synth_data.npy", allow_pickle=True).item()
-data_field = ift.Field.from_raw(position_space, data.val)
-# likelihood = ift.PoissonianEnergy(data_field) @ signal_response
-exit()
-mock = np.zeros(position_space.shape)
-mock[50, 50] = 190800
-mock[500, 500] = 190800
-mock[700, 700] = 190800
-mock = ift.makeField(position_space, mock)
-fa = ift.FieldAdapter(position_space,"signal").adjoint
-mock = fa(mock)
-mock_sr = response(mock)
-factor = data_field.val[499,499]/mock_sr.val[499,499]
-print("GT value: ",mock_sr.val[499,499])
-print("Factor: ",factor)
-print("Residual: ", data_field.val[499,499]-mock_sr.val[499,499])
-mock_sr_scaled = mock_sr*factor
-print("Scaled Value: ", mock_sr_scaled.val[499,499])
-res = data_field-mock_sr_scaled
-print("No residual: ", res.val[499,499])
+signal_response = response(signal_dl)
 
-xu.plot_single_psf(data_field, "synth_data_log.png", logscale=True, vmin=1)
-xu.plot_single_psf(mock_sr, f"app_gt_log_ih_{inhomogen}.png", logscale=True, vmin=1)
-xu.plot_single_psf(ift.abs(res), f"res_{inhomogen}.png", logscale=True, vmin=1)
-exit()
+data = np.load("../data/npdata/synth_data_130.npy", allow_pickle=True).item()
+data_field = ift.Field.from_raw(position_space, data.val)
+
+#mock = np.zeros(position_space.shape)
+#mock[50, 50] = 190800
+#mock[500, 500] = 190800
+#mock[700, 700] = 190800
+#mock = ift.makeField(position_space, mock)
+#fa = ift.FieldAdapter(position_space,"signal").adjoint
+#mock = fa(mock)
+#mock_sr = response(mock)
+#mock_sr = mock_sr.clip(a_min=0, a_max=np.max(mock_sr.val))
+
+# data_field = ift.random.current_rng().poisson(mock_sr.val.astype(np.float64))
+# data_field = ift.makeField(position_space, data_field)
+likelihood = ift.PoissonianEnergy(data_field) @ signal_response
+# factor = data_field.val[499,499]/mock_sr.val[499,499]
+# print("GT value: ",mock_sr.val[499,499])
+# print("Factor: ",factor)
+# print("Residual: ", data_field.val[499,499]-mock_sr.val[499,499])
+# mock_sr_scaled = mock_sr*factor
+# print("Scaled Value: ", mock_sr_scaled.val[499,499])
+# res = data_field-mock_sr_scaled
+# print("No residual: ", res.val[499,499])
+
+# xu.plot_single_psf(data_field, "synth_data_log.png", logscale=True, vmin=1)
+# xu.plot_single_psf(mock_sr, f"app_gt_log_ih_{inhomogen}.png", logscale=True, vmin=1)
+# xu.plot_single_psf(ift.abs(res), f"res_{inhomogen}.png", logscale=True, vmin=1)
 # End of Loop
 ic_sampling = ift.AbsDeltaEnergyController(**cfg['ic_sampling'])
 ic_sampling_nl = ift.AbsDeltaEnergyController(**cfg['ic_sampling_nl'])
@@ -81,16 +91,15 @@ n_samples = cfg['Nsamples']
 samples = ift.optimize_kl(
     likelihood,
     global_it,
-    n_samples,
+    0,
     minimizer,
-    ic_sampling,
-    minimizer_sampling,
+    None,
+    None,
     plottable_operators={
         "signal": transpose@signal,
     },
-    output_directory="synth_rec_inhom",
+    output_directory="synth_rec_inhom_130_nocut_take2",
     initial_position=pos,
-    comm=xu.library.mpi.comm,
     overwrite=True,
     resume=True
 )
