d7e3b12a1d5b692f2a08dae5173f0edbcf57337c
Vincent Eberle <veberle@mpa-garching.mpg.de>
2022-05-09 12:21:13 +0200

    OverlapAdd: fixed operator

--- xubik/src/operators/convolution_operators.py
+++ xubik/src/operators/convolution_operators.py
@@ -139,3 +139,76 @@ class OverlapAddConvolver(ift.LinearOperator):
         cut = MarginZeroPadder(domain[0], ((oa_back.domain.shape[0] - domain.shape[0])//2), space=0).adjoint
         res = cut @ oa_back.adjoint @ convolved
         return res
+
+    # @staticmethod
+    # def cut_by_value(domain, kernels_arr, n margin):
+
+    #     return OverlapAddConvolver()
+
+    # @staticmethod
+    # def force(domain, kernels_arr, n, margin):
+    #     return res
+
+class OAConvolver(ift.LinearOperator):
+    """
+    Performing a approximation to an inhomogeneous convolution,
+    by OverlapAdd convolution with different kernels and bilinear
+    interpolation of the result. In the case of one patch this simplifies
+    to a regular Fourier domain convolution.
+
+    Parameters:
+    -----------
+    domain: DomainTuple.
+        Domain of the Operator
+    kernels_arr: np.array
+        Array containing the different kernels for the inhomogeneos convolution
+    n: int
+        Number of patches
+    margin: int
+        Size of the margin. Number of pixels on one boarder.
+
+    """
+    def __init__(self, domain, kernels_arr, n, margin):
+        self._domain = ift.makeDomain(domain)
+        self._op = self._build_op(domain, kernels_arr, n, margin)
+        self._target = self._op.target
+        self._capability = self.TIMES | self.ADJOINT_TIMES
+        # TODO check for area cut out => only 0
+        # TODO normlize
+
+    def apply(self, x, mode):
+        self._check_input(x, mode)
+        if mode == self.TIMES:
+            res = self._op(x)
+        else:
+            res = self._op.adjoint(x)
+        return res
+
+    @staticmethod
+    def _build_op(domain, kernels_arr, n, margin):
+        domain = ift.makeDomain(domain)
+        marginmaker = MarginZeroPadder(domain[0], margin, space=0)
+        oa = OverlapAdd(marginmaker.target[0], n, margin)
+        zp = MarginZeroPadder(oa.target, margin, space=1)
+        padded = zp @ oa @ marginmaker
+
+        cutter = ift.FieldZeroPadder(
+            padded.target, kernels_arr.shape[1:], space=1, central=True
+        ).adjoint
+        kernels_b = ift.Field.from_raw(cutter.domain, kernels_arr)
+        # FIXME This is errorprone => check domains and distances in init
+        kernels = cutter(kernels_b)
+        print("kernel_domain",kernels.domain)
+        # FIXME Normalize here
+        convolved = convolve_field_operator(kernels, padded, space=1)
+        uspace = ift.UnstructuredDomain(64)
+        sp = ift.RGSpace([256,256], distances=domain[0].distances)
+        pad_space = ift.makeDomain([uspace, sp])
+        zp2 = MarginZeroPadder(pad_space, 320, space=1)
+        convolved = zp2.adjoint(convolved)
+        weights = ift.makeOp(get_weights(convolved.target))
+        weighted = weights @ convolved
+        oa_back = OverlapAdd(domain[0], n, 0)
+        # cut = MarginZeroPadder(domain[0], ((oa_back.domain.shape[0] - domain.shape[0])//2), space=0).adjoint
+        res = oa_back.adjoint @ weighted
+        return res
