9f5471bc413cac6e84e8edbbfb7945d23dba297b
Vincent Eberle <veberle@mpa-garching.mpg.de>
2022-05-25 21:24:23 +0200

    extra check and fix dtype

--- xubik/src/operators/bilinear_interpolation.py
+++ xubik/src/operators/bilinear_interpolation.py
@@ -4,7 +4,9 @@ import nifty8 as ift
 
 def _bilinear_weights(domain):
     psize = domain.shape[0]
-    a = np.linspace(0, 1, int(psize/2))
+    if psize/2 != int(psize/2):
+        raise ValueError("this should happen")
+    a = np.linspace(0, 1, int(psize/2), dtype="float64")
     b = np.concatenate([a, np.flip(a)])
     c = np.outer(b,b)
     return ift.Field.from_raw(domain, c)
