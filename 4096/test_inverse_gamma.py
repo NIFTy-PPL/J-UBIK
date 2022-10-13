0ff27e4283a273d3339889b2a810c2c1a958ef39
Vincent Eberle <veberle@mpa-garching.mpg.de>
2022-10-13 14:00:32 +0200

    remove 4096

--- 4096/test_inverse_gamma.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import nifty8 as ift
-
-position_space = ift.RGSpace(15)
-inverse_gamma = ift.InverseGammaOperator(position_space, 2, 2, 2)
-xi = ift.from_random(inverse_gamma.domain)
-ift.extra.check_operator(inverse_gamma, xi)
