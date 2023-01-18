e1f728176bd038e6a8ca8807eadf38a7ddba14da
Philipp Frank <philipp@mpa-garching.mpg.de>
2023-01-18 17:01:40 +0100

    fix vmap kernel interpolation

--- src/operators/psf_interpolation.py
+++ src/operators/psf_interpolation.py
@@ -1,8 +1,11 @@
 import nifty8 as ift
 import numpy as np
+import jax
 import jax.numpy as jnp
 
 from jax.scipy.ndimage import map_coordinates
+from .convolution_operators import OAnew
+from .adg.nifty_convolve import get_convolve
 
 def to_r_phi(cc):
     """
@@ -19,6 +22,33 @@ def to_ra_dec(rp):
     x, y = rp[0]*jnp.cos(rp[1]), rp[0]*jnp.sin(rp[1])
     return jnp.array([x, y])
 
+def get_interpolation_weights(rs, r):
+    dr = rs[1:] - rs[:-1]
+
+    def _get_wgt_front(i):
+        res = jnp.zeros_like(rs)
+        res = res.at[0].set(1.)
+        return res
+    res = jax.lax.cond(r < rs[0], _get_wgt_front, lambda _: jnp.zeros_like(rs), 0)
+
+    def _get_wgt_back(i):
+        res = jnp.zeros_like(rs)
+        res = res.at[rs.size-1].set(1.)
+        return res    
+    res += jax.lax.cond(r >= rs[rs.size-1], _get_wgt_back, lambda _: jnp.zeros_like(rs), 0)
+
+    def _get_wgt(i):
+        res = jnp.zeros_like(rs)
+        wgt = (r - rs[i]) / dr[i]
+        res = res.at[i].set(1. - wgt)
+        res = res.at[i+1].set(wgt)
+        return res
+    for i in range(rs.size - 1):
+        res += jax.lax.cond((rs[i]<r)*(rs[i+1]>=r), 
+                _get_wgt, (lambda _: jnp.zeros_like(rs)), i)
+    return res
+
+
 def find_interpolate_index_r(rs, r):
     """
     Find indices and weights for bilinear interpolation of the psfs
@@ -28,8 +58,16 @@ def find_interpolate_index_r(rs, r):
     """
     upper = rs[1:]
     lower = rs[:-1]
-    i_upper = jnp.where(upper < r)[0]
-    i_lower = jnp.where(lower >= r)[0]
+    i_upper = []
+    i_lower = []
+    for i in range(upper.size):
+        i_upper += jax.lax.cond(upper[i] < r, (lambda i: [i,]), (lambda _: [-1]), i)
+        i_lower += jax.lax.cond(lower[i] >=r, (lambda i: [i,]), (lambda _: [-1]), i)
+    i_upper = jnp.array(i_upper)
+    i_upper = i_upper[i_upper != -1]
+    
+    #i_upper = jnp.where(upper < r)[0]
+    #i_lower = jnp.where(lower >= r)[0]
     # If `r` larger then biggest radius extrapolate psf
     if i_upper.size == upper.size:
         return (rs.size-1,), (1.,)
@@ -103,6 +141,7 @@ def get_psf(psfs, rs, patch_center_ids, patch_deltas, pointing_center,
     # Sort in ascending radii
     sort = np.argsort(rs)
     rs = rs[sort]
+    rs = jnp.array(rs)
     psfs = psfs[sort]
     patch_center_ids = patch_center_ids[sort]
 
@@ -129,21 +168,20 @@ def get_psf(psfs, rs, patch_center_ids, patch_deltas, pointing_center,
         cc -= pointing_center
         rp = to_r_phi(cc)
         # Find and select psfs required for given radius
-        inds, wgts = find_interpolate_index_r(rs, rp[0])
+        #inds, wgts = find_interpolate_index_r(rs, rp[0])
+        wgts = get_interpolation_weights(rs, rp[0])
 
         # Rotate requested psf slice to align with patch
         int_coords = jnp.stack((dra, ddec), axis = 0)
         int_coords = to_r_phi(int_coords)
-        print(cc[1], jnp.pi)
         int_coords = int_coords.at[1].set(int_coords[1] - rp[1])
         int_coords = to_ra_dec(int_coords)
 
         # Transform psf slice coordinates into patch index coordinates and
         # bilinear interpolate onto slice
         res = jnp.zeros(int_coords.shape[1:])
-        for ii, ww in zip(inds, wgts):
-            ids = patch_center_ids[ii]
-            pp = psfs[ii]
+        print(wgts)
+        for ids, pp, ww in zip(patch_center_ids, psfs, wgts):
             query_ids = to_patch_coordinates(int_coords, ids, patch_deltas)
             int_res = map_coordinates(pp, query_ids, order=1, mode='nearest')
             res += ww*int_res
@@ -152,22 +190,12 @@ def get_psf(psfs, rs, patch_center_ids, patch_deltas, pointing_center,
 
     return psf
 
-def psf_convolve_operator(domain, lower_radec, obs_infos, msc_infos):
-    # NOTE: Assumes the repository "https://gitlab.mpcdf.mpg.de/pfrank/adg.git"
-    # to be cloned and located in a folder named "adg" within the folder of this
-    # python file.
-    from .adg.nifty_convolve import get_convolve
+def get_psf_func(domain, lower_radec, obs_infos):
     psfs = obs_infos['psfs']
     rs = obs_infos['rs']
     patch_center_ids = obs_infos['patch_center_ids']
     patch_deltas = obs_infos['patch_deltas']
     pointing_center = obs_infos['pointing_center']
-    c = msc_infos['c']
-    q = msc_infos['q']
-    b = msc_infos['b']
-    min_m0 = msc_infos['min_m0']
-    linear = msc_infos['Linear']
-    local = True
 
     if not isinstance(domain, ift.RGSpace):
         raise ValueError
@@ -176,75 +204,51 @@ def psf_convolve_operator(domain, lower_radec, obs_infos, msc_infos):
 
     upper_radec = (tuple(ll+ss*dd for ll,ss,dd in 
                    zip(lower_radec, domain.shape, domain.distances)))
-    radec_limits = tuple(lower_radec, upper_radec)
+    radec_limits = (lower_radec, upper_radec)
+    return get_psf(psfs, rs, patch_center_ids, patch_deltas, 
+                   pointing_center, radec_limits)
 
-    func_psf = get_psf(psfs, rs, patch_center_ids, patch_deltas, 
-                       pointing_center, radec_limits)
+def psf_convolve_operator(domain, lower_radec, obs_infos, msc_infos):
+    # NOTE: Assumes the repository "https://gitlab.mpcdf.mpg.de/pfrank/adg.git"
+    # to be cloned and located in a folder named "adg" within the folder of this
+    # python file.
+    
+    c = msc_infos['c']
+    q = msc_infos['q']
+    b = msc_infos['b']
+    min_m0 = msc_infos['min_m0']
+    linear = msc_infos['linear']
+    local = True
 
+    func_psf = get_psf_func(domain, lower_radec, obs_infos)
     return get_convolve(domain, func_psf, c, q, b, min_m0, linear, local)
 
-def test_psf():
-    import pylab as plt
+def psf_lin_int_operator(domain, npatch, lower_radec, obs_infos, margfrac=0.1):
+    func_psf = get_psf_func(domain, lower_radec, obs_infos)
 
-    max_radec = (2.,2.)
-    npix_x, npix_y = 128, 128
+    shp = domain.shape
+    dist = domain.distances
+    for ss in shp:
+        if ss%npatch != 0:
+            raise ValueError
+        if ss%2 != 0:
+            raise ValueError
+    patch_shp = tuple(ss//npatch for ss in shp)
+    c_p = ((np.arange(ss) - ss/2 + 0.5)*dd for ss,dd in zip(shp, dist))
+    c_p = np.meshgrid(*c_p, indexing='ij')
+    d_ra = c_p[0]
+    d_dec = c_p[1]
 
-    ra = np.arange(npix_x) / npix_x * max_radec[0]
-    ra -= 0.5*max_radec[0]
-    dec = np.arange(npix_y) / npix_y * max_radec[1]
-    dec -= 0.5*max_radec[1]
-    ra, dec = ra[1:], dec[1:]
-    print(ra)
-    print(dec)
+    centers = (np.array([(i*ss + ss/2 + 0.5)*dd for i in range(npatch)]) for 
+               ss, dd in zip(patch_shp, dist))
+    centers = np.meshgrid(*centers, indexing='ij')
+    c_ra = centers[0].flatten()
+    c_dec = centers[1].flatten()
 
-
-
-    sig = 0.1
-    def func(r, dx,dy):
-        tm = 3.*dy**2 - dx**2 - dy
-        dr = np.sqrt((dx/(1.+2.*r**2))**2 + tm**2)
-        return np.exp(-0.5*(dr/sig)**2)
-
-    rs = np.array([0., 0.1, 0.5, 0.7, 1.])
-
-    nx = 128*2
-    ny = 128*2
-    dra = np.linspace(-1.,1., num = nx)
-    ddec = np.linspace(-1.,1., num = ny)
-    dx = dra[1] - dra[0]
-    dy = ddec[1] - ddec[0]
-    dra, ddec = np.meshgrid(dra, ddec, indexing='ij')
-    #dradecs = np.stack((dra, ddec), axis = -1)
-
-    psfs = list([func(rr, dra, ddec) for rr in rs])
-    psfs = np.stack(psfs, axis = 0)
-
-    #for pp,rr in zip(psfs,rs):
-    #    plt.imshow(pp.T, origin='lower')
-    #    plt.title(f'radius = {rr}')
-    #    plt.show()
-
-    patch_centers = np.outer(np.ones_like(rs), np.array([128, 128]))
-    patch_deltas = (dx, dy)
-    center = (1.,1.)
-    radec_limits = ((0.,0.), (2.,2.))
-
-
-    func_psf = get_psf(psfs, rs, patch_centers, patch_deltas, center, radec_limits)
-    #func_psf = get_psf(rs, dradecs, psfs, center, max_radec)
-
-    ddra, dddec = jnp.meshgrid(ra, dec, indexing='ij')
-    ra, dec = 1.5, .5
-    mypsf = func_psf(ra, dec, ddra, dddec)
-    im = plt.imshow(mypsf.T, origin='lower', vmin = 0., vmax = 1.)
-    plt.colorbar(im)
-    plt.show()
-
-    test_r = np.sqrt((ra-center[0])**2 + (dec-center[1])**2)
-    gtpsf = func(test_r, ddra, dddec)
-    im = plt.imshow(gtpsf.T, origin='lower', vmin = 0., vmax = 1.)
-    plt.colorbar(im)
-    plt.show()
-
-
-test_psf()
+    patch_psfs = list([func_psf(ra, dec, d_ra, d_dec) for ra, dec in 
+                       zip(c_ra, c_dec)])
+    patch_psfs = np.array(patch_psfs)
+    margin = max((int(np.ceil(margfrac*ss)) for ss in patch_shp))
+    print(margin)
+    op = OAnew(domain, patch_psfs, len(patch_psfs), margin)
+    return op
