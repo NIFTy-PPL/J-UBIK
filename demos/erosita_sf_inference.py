e11b843add9583ee390efc3c53d93413191cd510
wmarg <margret@mpa-garching.mpg.de>
2023-01-18 10:59:23 +0100

    eROSITA3.8: Config

--- demos/erosita_sf_inference.py
+++ demos/erosita_sf_inference.py
@@ -1,49 +1,67 @@
 import argparse
-import os.path
-
+import os
+import sys
 import nifty8 as ift
 import xubik0 as xu
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+print(sys.path)
+print
 from src.library.erosita_observation import ErositaObservation
-import matplotlib.colors as colors
-
-parser = argparse.ArgumentParser()
-parser.add_argument("obs_path", type=str, nargs='?', default="../data/LMC_SN1987A/")
-parser.add_argument("plotting", type=bool, nargs='?', default=False)
-args = parser.parse_args()
 
 if __name__ == "__main__":
-    emin = 1.0
-    emax = 2.3
-
-    obs_path = args.obs_path  # Folder that gets mounted to the docker
-    input_filenames = ['fm00_700203_020_EventList_c001.fits']
-    output_filename = "processed_LMC_SN1987A_data.fits"
-    exposure_filename = "expmap_LMC_SN1987A.fits"
+    cfg = xu.get_cfg("demos/eROSITA_config.yaml")
+    # File Location
+    file_info = cfg['files']
+    obs_path = file_info['obs_path']
+    input_filenames = file_info['input']
+    output_filename = file_info['output']
+    exposure_filename = file_info['exposure']
     observation_instance = ErositaObservation(input_filenames, output_filename, obs_path)
+
+    # Grid Info
+
+    grid_info = cfg['grid']
+    e_min = grid_info['energy_bin']['e_min']
+    e_max = grid_info['energy_bin']['e_max']
+    npix = grid_info['npix']
+
+    # Telescope Info
+
+    tel_info = cfg['telescope']
+    tm_id = tel_info['tm_id']
+
 
     log = 'Output file {} already exists and is not regenerated. If the observations parameters shall be changed ' \
           'please delete or rename the current output file.'
 
     if not os.path.exists(os.path.join(obs_path, output_filename)):
-        observation = observation_instance.get_data(emin=emin, emax=emax, image=True, rebin=80, size=3240, pattern=15,
-                                                    telid=1)
+        observation = observation_instance.get_data(emin=e_min, emax=e_max, image=True, rebin=tel_info['rebin'],
+                                                    size=npix, pattern=tel_info['pattern'],
+                                                    telid=tm_id)
     else:
         print(log.format(os.path.join(obs_path, output_filename)))
 
     observation_instance = ErositaObservation(output_filename, output_filename, obs_path)
 
     # Exposure
-    if not os.path.exists(os.path.join(obs_path, output_filename)):
-        observation_instance.get_exposure_maps(output_filename, emin, emax, mergedmaps=exposure_filename)
+    if not os.path.exists(os.path.join(obs_path, exposure_filename)):
+        observation_instance.get_exposure_maps(output_filename, e_min, e_max, mergedmaps=exposure_filename)
 
     else:
         print(log.format(os.path.join(obs_path, output_filename)))
-
-    if args.plotting:
-        observation_instance.plot_fits_data(output_filename, "combined_out_08_1_no_imm", slice=(1100, 2000, 800, 2000),
-                                            dpi=800)
-        observation_instance.plot_fits_data(exposure_filename, "exposure_LMC_SN1987A.png",
-                                            slice=(1100, 2000, 800, 2000), dpi=800)
+    # Plotting
+    plot_info = cfg['plotting']
+    if plot_info['enabled']:
+        observation_instance.plot_fits_data(output_filename,
+                                            os.path.splitext(output_filename)[0],
+                                            slice=tuple(plot_info['slice']),
+                                            dpi=plot_info['dpi'])
+        observation_instance.plot_fits_data(exposure_filename,
+                                            f'{os.path.splitext(exposure_filename)[0]}.png',
+                                            slice=tuple(plot_info['slice']),
+                                            dpi=plot_info['dpi'])
 
     data = observation_instance.load_fits_data(output_filename)[0].data
     exposure = observation_instance.load_fits_data(exposure_filename)[0].data
