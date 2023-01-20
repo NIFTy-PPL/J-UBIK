45c15fc18135d8b1bcc89d8ce22c2808fd57a5ed
Vincent Eberle <veberle@mpa-garching.mpg.de>
2023-01-20 19:24:03 +0100

    fix import

--- demos/demos_psfs/test_psfs.py
+++ demos/demos_psfs/test_psfs.py
@@ -2,8 +2,8 @@ import numpy as np
 import jax.numpy as jnp
 import nifty8 as ift
 
-from xubik0.operators.psf_interpolation import (psf_convolve_operator, 
-        psf_lin_int_operator, get_psf)
+from xubik0.operators.convolve_utils import (psf_convolve_operator,
+                                             psf_lin_int_operator, get_psf)
 
 def test_psf():
     import pylab as plt
