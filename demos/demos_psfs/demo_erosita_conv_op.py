8a0ae44e75877d8cb9f44f18ad16218f17b6c6d4
Vincent Eberle <veberle@mpa-garching.mpg.de>
2023-01-20 19:41:09 +0100

    fix last file path

--- demos/demos_psfs/demo_erosita_conv_op.py
+++ demos/demos_psfs/demo_erosita_conv_op.py
@@ -25,7 +25,8 @@ def get_kernels_and_sources(domain, psf_func):
     rnds = ift.makeField(domain, rnds)
     return ift.makeField(domain, res), rnds
 
-dir_path= "tm1/bcf/"
+
+dir_path = "data/tm1/bcf/"
 fname = ["tm1_2dpsf_190219v05.fits", "tm1_2dpsf_190220v03.fits"]
 
 file = dir_path + fname[0]
