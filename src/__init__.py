d9ed5b244e7cd586cf3be41272b3d5014ca79693
Philipp Frank <philipp@mpa-garching.mpg.de>
2023-01-20 11:28:04 +0100

    remove double imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,9 @@
 from .library.utils import (get_mask_operator, prior_sample_plotter, get_norm, get_normed_exposure,
                             get_norm_exposure_patches,get_data_domain, get_cfg, convolve_operators,
                             convolve_field_operator, Transposer, energy_binning, save_rgb_image_to_fits,
-                            transform_loglog_slope_pars, is_subdomain, save_to_fits, rgb_plotting_callback)
+                            transform_loglog_slope_pars, is_subdomain, save_to_fits, rgb_plotting_callback,
+                            coord_center, get_radec_from_xy, get_psfpatches, get_synth_pointsource)
 from .library.plot import plot_slices, plot_result, plot_fused_data, plot_rgb_image, plot_image_from_fits
-from .library.utils import (get_mask_operator, prior_sample_plotter, get_norm, get_normed_exposure,
-                            get_data_domain, get_cfg, convolve_operators, convolve_field_operator,
-                            Transposer, coord_center, get_radec_from_xy, get_psfpatches, get_synth_pointsource)
 from .library.plot import plot_slices, plot_result, plot_single_psf, plot_psfset
 from .library import mpi
 from .library.special_distributions import InverseGammaOperator
