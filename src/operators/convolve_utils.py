b7bf52d9836343e8066637f00437f2f3fac1a171
Vincent Eberle <veberle@mpa-garching.mpg.de>
2023-01-20 11:27:06 +0100

    move bilinear_weights to convolution_op

--- src/operators/convolve_utils.py
+++ src/operators/convolve_utils.py
@@ -248,16 +248,3 @@ def get_gaussian_kernel(width, domain):
     res = explode_pad.adjoint(kern)
     return res
 
-def _bilinear_weights(domain):
-    psize = domain.shape[0]
-    if psize/2 != int(psize/2):
-        raise ValueError("this should happen")
-    a = np.linspace(0, 1, int(psize/2), dtype="float64")
-    b = np.concatenate([a, np.flip(a)])
-    c = np.outer(b,b)
-    return ift.Field.from_raw(domain, c)
-
-def get_weights(domain):
-    weights = _bilinear_weights(domain[1])
-    explode = ift.ContractionOperator(domain, spaces=0).adjoint
-    return explode(weights)
