52febb7cadce4874cb95bda59d57dd51811da626
Vincent Eberle <veberle@mpa-garching.mpg.de>
2023-01-24 11:08:49 +0100

    inconsistency in FOV

--- demos/erosita_inference.py
+++ demos/erosita_inference.py
@@ -26,7 +26,8 @@ if __name__ == "__main__":
     except:
         cfg = xu.get_cfg('demos/' + config_filename)
     fov = cfg['telescope']['fov']
-    rebin = math.floor(20 * fov // cfg['grid']['npix'])
+    rebin = math.floor(20 * fov // cfg['grid']['npix'])  # FIXME USE DISTANCES!
+    distance_1 = fov/cfg['grid']['npix']
     mock_run = cfg['mock']
 
     # File Location
@@ -95,6 +96,9 @@ if __name__ == "__main__":
     # Places the pointing in the center of the image (or equivalently defines
     # the image to be centered around the pointing).
     dom = sky_model.extended_space
+    distance_2 = dom.distances
+    print(distance_1,distance_2)
+    exit()
     center = tuple(0.5*ss*dd for ss,dd in zip(dom.shape, dom.distances))
     energy = cfg['psf']['energy']
     conv_op = psf_file.make_psf_op(energy, center, sky_model.extended_space,
