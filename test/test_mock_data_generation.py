37de265b2fda4102d7ae468d9d18289511f2756f
Vincent Eberle <veberle@mpa-garching.mpg.de>
2023-02-03 12:17:26 +0100

    simplify mock test

--- test/test_mock_data_generation.py
+++ test/test_mock_data_generation.py
@@ -1,58 +1,35 @@
-import os
-import sys
 import numpy as np
-import unittest
+import pytest
 
 import nifty8 as ift
 import xubik0 as xu
 
-currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
-from src.library.utils import generate_mock_data, get_cfg
 
-
-def test(config_filename):
-    try:
-        config_path = config_filename
-        cfg = get_cfg(config_path)
-    except:
-        config_path = 'demos/' + config_filename
-        cfg = get_cfg(config_path)
-    sky_model = xu.SkyModel(config_path)
-    exposures = [None, ift.makeField(sky_model.position_space,
-                                     np.random.choice([0, 1], sky_model.position_space.shape))]
-    padders = [None, sky_model.pad]
-    ift.logger.info(sky_model.pad.domain)
-    psf_kernels = [None, ift.makeField(sky_model.extended_space,
-                                       np.random.choice([0.0, 1.0], sky_model.extended_space.shape))]
-    vars = [None, 5]
-    output_directories = [None, 'test_mock_data_generation/']
-    for exposure in exposures:
-        for padder in padders:
-            for psf_kernel in psf_kernels:
-                for var in vars:
-                    for output_directory in output_directories:
-                        try:
-                            generate_mock_data(sky_model=sky_model,
-                                               exposure=exposure,
-                                               pad=padder,
-                                               psf_kernel=psf_kernel,
-                                               var=var,
-                                               output_directory=output_directory)
-                        except ValueError as v:
-                            if var is None and psf_kernel is None:
-                                pass
-                            elif padder is None and sky_model.position_space != sky_model.extended_space:
-                                pass
-                            else:
-                                print(f'Unexpected Value Error {v} for combination: {exposure}, {padder}, {psf_kernel},'
-                                      f'{var}, {output_directory}')
-                        except Exception as e:
-                            print(f'Unexpected Exception {e} for combination: {exposure}, {padder}, {psf_kernel},'
-                                   f'{var}, {output_directory}')
-
+pmp = pytest.mark.parametrize
 
 cfg_filename = 'eROSITA_config.yaml'
-test(cfg_filename)
+config_path = 'demos/' + cfg_filename  # FIXME cfg in testdir!!
+cfg = xu.get_cfg(config_path)
+sky_model = xu.SkyModel(config_path)
 
+psf_kernel = ift.makeField(sky_model.extended_space,
+                           np.random.choice([0.0, 1.0], sky_model.extended_space.shape))
+exposure = ift.makeField(sky_model.position_space,
+                         np.random.choice([0, 1], sky_model.position_space.shape))
+output_directory = 'test_mock_data_generation/'
+
+@pmp('gauss_var', [None, 5])
+@pmp('psf_kernel', [None, psf_kernel])
+@pmp('exposure', [None, exposure])
+@pmp('padder', [sky_model.pad])
+def test(psf_kernel, gauss_var, exposure, padder):
+    if gauss_var is None and psf_kernel is None:
+        pass
+    else:
+        ift.logger.info(sky_model.pad.domain)
+        xu.generate_mock_data(sky_model=sky_model,
+                              exposure=exposure,
+                              pad=padder,
+                              psf_kernel=psf_kernel,
+                              var=gauss_var,
+                              output_directory=output_directory)
