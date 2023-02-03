f8bcfd12912c892ffb17ee709df91bee5eb259e5
Vincent Eberle <veberle@mpa-garching.mpg.de>
2023-02-03 12:24:50 +0100

    formating

--- test/test_mock_data_generation.py
+++ test/test_mock_data_generation.py
@@ -5,24 +5,23 @@ import nifty8 as ift
 import xubik0 as xu
 
 
-pmp = pytest.mark.parametrize
-
 cfg_filename = 'eROSITA_config.yaml'
 config_path = 'demos/' + cfg_filename  # FIXME cfg in testdir!!
 cfg = xu.get_cfg(config_path)
 sky_model = xu.SkyModel(config_path)
 
-psf_kernel = ift.makeField(sky_model.extended_space,
-                           np.random.choice([0.0, 1.0], sky_model.extended_space.shape))
-exposure = ift.makeField(sky_model.position_space,
-                         np.random.choice([0, 1], sky_model.position_space.shape))
+psf_arr = np.random.choice([0.0, 1.0], sky_model.extended_space.shape)
+psf_kernel = ift.makeField(sky_model.extended_space, psf_arr)
+exposure_arr = np.random.choice([0, 1], sky_model.position_space.shape)
+exposure = ift.makeField(sky_model.position_space, exposure_arr)
 output_directory = 'test_mock_data_generation/'
 
+pmp = pytest.mark.parametrize
 @pmp('gauss_var', [None, 5])
 @pmp('psf_kernel', [None, psf_kernel])
 @pmp('exposure', [None, exposure])
 @pmp('padder', [sky_model.pad])
-def test(psf_kernel, gauss_var, exposure, padder):
+def test_generate_mock_data(psf_kernel, gauss_var, exposure, padder):
     if gauss_var is None and psf_kernel is None:
         pass
     else:
