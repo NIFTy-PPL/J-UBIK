51406080d4e37e20e29b38bde5ff73ff4f2ca89b
wmarg <margret@mpa-garching.mpg.de>
2023-02-03 15:29:21 +0100

    Make operator input more flexible

--- src/library/diagnostics.py
+++ src/library/diagnostics.py
@@ -8,15 +8,15 @@ import nifty8 as ift
 from src.library.utils import save_rgb_image_to_fits
 
 
-def uncertainty_weighted_residual_image_from_file(sl_path_base, ground_truth_path, sky_model=None,
+def uncertainty_weighted_residual_image_from_file(sl_path_base, ground_truth_path, op=None,
                                                   output_dir_base=None):
+    #FIXME: weighted residual in data space
     mpi_master = ift.utilities.get_MPI_params()[3]
-    _, _, sky = sky_model.create_sky_model()
     sl = ift.ResidualSampleList.load(sl_path_base)
-    mean, var = sl.sample_stat(sky)
+    mean, var = sl.sample_stat(op)
     with open(ground_truth_path, "rb") as f:
-        rec = pickle.load(f)
-    wgt_res = np.abs(mean.val-rec.val)/np.sqrt(var.val)
+         d = pickle.load(f)
+    wgt_res = np.abs((mean-d).val)/np.sqrt(var.val)
     wgt_res = ift.Field.from_raw(domain=mean.domain, arr=wgt_res)
     if mpi_master and output_dir_base is not None:
         with open(f'{output_dir_base}.pkl', 'wb') as file:
