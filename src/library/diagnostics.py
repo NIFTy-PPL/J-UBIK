9c0ef841b6d136c2c1d544b513a8188c6d446bc7
wmarg <margret@mpa-garching.mpg.de>
2023-02-09 15:34:23 +0100

    Add correct title for plot

--- src/library/diagnostics.py
+++ src/library/diagnostics.py
@@ -9,7 +9,8 @@ from src.library.utils import save_rgb_image_to_fits
 
 def get_uncertainty_weighted_measure(sl, op=None,
                                      reference=None,
-                                     output_dir_base=None):
+                                     output_dir_base=None,
+                                     title=''):
     mpi_master = ift.utilities.get_MPI_params()[3]
     mean, var = sl.sample_stat(op)
     if reference is None:
@@ -22,7 +23,7 @@ def get_uncertainty_weighted_measure(sl, op=None,
         save_rgb_image_to_fits(wgt_res, output_dir_base,
                                overwrite=True, MPI_master=mpi_master)
         p = ift.Plot()
-        p.add(wgt_res, title="Uncertainty weighted measure", norm=LogNorm())
+        p.add(wgt_res, title=f"Uncertainty weighted {title}", norm=LogNorm())
         p.output(name=f'{output_dir_base}.png')
     return wgt_res
 
@@ -30,11 +31,13 @@ def get_uncertainty_weighted_measure(sl, op=None,
 def signal_space_uwr_from_file(sl_path_base,
                                ground_truth_path,
                                sky_op,
-                               output_dir_base=None):
+                               output_dir_base=None,
+                               title='signal space UWR'):
     sl = ift.ResidualSampleList.load(sl_path_base)
     with open(ground_truth_path, "rb") as f:
          gt = pickle.load(f)
-    wgt_res = get_uncertainty_weighted_measure(sl, sky_op, gt, output_dir_base)
+    wgt_res = get_uncertainty_weighted_measure(sl, sky_op, gt, output_dir_base,
+                                               title=title)
     return wgt_res
 
 
@@ -42,19 +45,23 @@ def data_space_uwr_from_file(sl_path_base,
                              data_path,
                              sky_op,
                              response_path,
-                             output_dir_base=None):
+                             output_dir_base=None,
+                             title='data space UWR'):
     sl = ift.ResidualSampleList.load(sl_path_base)
     with open(data_path, "rb") as f:
          d = pickle.load(f)
     with open(response_path, "rb") as f:
          R = pickle.load(f)
-    wgt_res = get_uncertainty_weighted_measure(sl, R @ sky_op, d, output_dir_base)
+    wgt_res = get_uncertainty_weighted_measure(sl, R @ sky_op, d, output_dir_base,
+                                               title=title)
     return wgt_res
 
 
 def signal_space_uwm_from_file(sl_path_base,
                                sky_op,
-                               output_dir_base=None):
+                               output_dir_base=None,
+                               title='signal space UWM'):
     sl = ift.ResidualSampleList.load(sl_path_base)
-    wgt_mean = get_uncertainty_weighted_measure(sl, sky_op, output_dir_base=output_dir_base)
+    wgt_mean = get_uncertainty_weighted_measure(sl, sky_op, output_dir_base=output_dir_base,
+                                                title=title)
     return wgt_mean
