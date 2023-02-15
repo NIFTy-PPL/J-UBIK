3cd2c7947d0fa51119d3730a25795e8325303272
Vincent Eberle <veberle@mpa-garching.mpg.de>
2023-02-15 15:24:56 +0100

    first draft

--- demos/eROSITA_evaluation.py
+++ demos/eROSITA_evaluation.py
@@ -2,14 +2,13 @@ import nifty8 as ift
 import xubik0 as xu
 
 if __name__ == "__main__":
-    config_filename= "eROSITA_config.yaml"
-    cfg = xu.get_cfg(config_filename) # FIXME OutputFilename
-
-    sl_path_base = ""
-    data_path = ""
-    sky_op = SkyModel(config_filename)
-    response_path ""
-    output_dir_base = ""
+    reconstruction_path = "path/to/dir/" #FIXME filepath
+    config_filename = "eROSITA_config.yaml"
+    sky_op = SkyModel(reconstruction_path + config_filename)
+    sl_path_base = reconstruction_path + "pickle/last" # NIFTy dependency
+    data_path = reconstruction_path + "data"
+    response_path = ""
+    output_dir_base = "diagnostics"
     xu.signal_space_uwr_from_file()
     xu.data_space_uwr_from_file()
     xu.signal_space_uwm_from_file()
