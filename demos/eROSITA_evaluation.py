9b643ba9fe73661ea4c0ff5714ad48fc4d2e50f4
wmarg <margret@mpa-garching.mpg.de>
2023-02-16 15:12:52 +0100

    Add postprocessing pipeline-

--- demos/eROSITA_evaluation.py
+++ demos/eROSITA_evaluation.py
@@ -1,15 +1,88 @@
+import numpy as np
+import pickle
+
 import nifty8 as ift
 import xubik0 as xu
 
+
 if __name__ == "__main__":
-    reconstruction_path = "path/to/dir/" #FIXME filepath
+    """This is the postprocessing pipeline for the eROSITA reconstruction:
+    The uncertainty weighted residual (UWR) as well as their distribution 
+    and the uncertainty weighted mean (UWM) are calculated. For mock data
+    additionally the UWR in signal space is calculated."""
+
+
+
+    # Paths -Set by user
+    reconstruction_path = "../rec_weak_points/" #FIXME filepath
     config_filename = "eROSITA_config.yaml"
-    sky_op = SkyModel(reconstruction_path + config_filename)
     sl_path_base = reconstruction_path + "pickle/last" # NIFTy dependency
-    data_path = reconstruction_path + "data"
-    response_path = ""
-    output_dir_base = "diagnostics"
-    xu.signal_space_uwr_from_file()
-    xu.data_space_uwr_from_file()
-    xu.signal_space_uwm_from_file()
-    xu.weighted_residual_distribution()
+    data_path = reconstruction_path + "diagnostics/mock_data_sky.pkl"
+    response_path = "" # FIXME response operator shall be loaded from path
+    output_dir_base = reconstruction_path + "diagnostics"
+    # Ground Truth path Only needed for mock run
+    ground_truth_path = reconstruction_path + "diagnostics/mock_sky.pkl"
+
+
+    # Config
+    cfg = xu.get_cfg(reconstruction_path + config_filename)
+    mock_run = cfg['mock']
+    mock_psf = cfg['mock_psf']
+    file_info = cfg['files']
+    output_filename = file_info['output']
+    obs_path = file_info['obs_path']
+    exposure_filename = file_info['exposure']
+
+
+    # Operators
+    # Sky
+    sky_model = xu.SkyModel(reconstruction_path + config_filename)
+    sky_dict = sky_model.create_sky_model()
+    # Repsonse # FIXME run
+    observation_instance = xu.ErositaObservation(output_filename, output_filename, obs_path)
+    exposure = observation_instance.load_fits_data(exposure_filename)[0].data
+    exposure_field = ift.makeField(sky_model.position_space, exposure)
+    padded_exposure_field = sky_model.pad(exposure_field)
+    exposure_op = ift.makeOp(padded_exposure_field)
+    mask = xu.get_mask_operator(exposure_field)
+    try:
+        with open(response_path, "rb") as f:
+            R = pickle.load(f)
+    except:
+        print('Not able to load response from file. Generating response from config ...')
+        if cfg['psf']['method'] in ['MSC', 'LIN']:
+            center = observation_instance.get_center_coordinates(output_filename)
+            psf_file = xu.eROSITA_PSF(cfg["files"]["psf_path"])  # FIXME: load from config
+
+            dom = sky_model.extended_space
+            center = tuple(0.5*ss*dd for ss, dd in zip(dom.shape, dom.distances))
+
+            energy = cfg['psf']['energy']
+            conv_op = psf_file.make_psf_op(energy, center, sky_model.extended_space,
+                                        conv_method=cfg['psf']['method'],
+                                        conv_params=cfg['psf'])
+
+        elif cfg['psf']['method'] == 'invariant':
+            if mock_psf:
+                conv_op = xu.get_gaussian_psf(sky_dict['sky'], var=cfg['psf']['gauss_var'])
+            else:
+                center = observation_instance.get_center_coordinates(output_filename)
+                psf_file = xu.eROSITA_PSF(cfg["files"]["psf_path"])
+                psf_function = psf_file.psf_func_on_domain('3000', center, sky_model.extended_space)
+                psf_kernel = psf_function(*center)
+                psf_kernel = ift.makeField(sky_model.extended_space, np.array(psf_kernel))
+                conv_op = xu.get_fft_psf_op(psf_kernel, sky_dict['sky'])
+        else:
+            raise NotImplementedError
+
+    R = mask @ sky_model.pad.adjoint @ exposure_op @ conv_op
+    if mock_run:
+        xu.signal_space_uwr_from_file(sl_path_base=sl_path_base, ground_truth_path=ground_truth_path,
+                                      sky_op=sky_dict['sky'], output_dir_base=output_dir_base+'/signal_space_uwr')
+    xu.data_space_uwr_from_file(sl_path_base=sl_path_base, data_path=data_path, sky_op=sky_dict['sky'],
+                                response_op=R, mask_op=mask, output_dir_base=output_dir_base+'/data_space_uwr')
+    xu.signal_space_uwm_from_file(sl_path_base=sl_path_base, sky_op=sky_dict['sky'],
+                                  output_dir_base=output_dir_base+'/uwm')
+    xu.weighted_residual_distribution(sl_path_base=sl_path_base, data_path=data_path, sky_op=sky_dict['sky'],
+                                      response_op=R, mask_op=mask,
+                                      output_dir_base=output_dir_base+'/res_distribution')
--- src/library/diagnostics.py
+++ src/library/diagnostics.py
@@ -5,19 +5,23 @@ from matplotlib.colors import LogNorm
 
 import nifty8 as ift
 
-from src.library.utils import save_rgb_image_to_fits
+from .utils import save_rgb_image_to_fits
 
 
 def get_uncertainty_weighted_measure(sl, op=None,
                                      reference=None,
                                      output_dir_base=None,
+                                     mask_op = None,
                                      title=''):
     mpi_master = ift.utilities.get_MPI_params()[3]
     mean, var = sl.sample_stat(op)
+    if mask_op is None:
+        mask_op = ift.ScalingOperator(domain=mean.domain, factor=1)
     if reference is None:
         wgt_res = mean / var.sqrt()
     else:
         wgt_res = (mean-reference).abs() / var.sqrt()
+    wgt_res = mask_op.adjoint(wgt_res)
     if mpi_master and output_dir_base is not None:
         with open(f'{output_dir_base}.pkl', 'wb') as file:
             pickle.dump(wgt_res, file)
@@ -46,13 +50,14 @@ def data_space_uwr_from_file(sl_path_base,
                              data_path,
                              sky_op,
                              response_op,
+                             mask_op,
                              output_dir_base=None,
                              title='data space UWR'):
     sl = ift.ResidualSampleList.load(sl_path_base)
     with open(data_path, "rb") as f:
          d = pickle.load(f)
-    wgt_res = get_uncertainty_weighted_measure(sl, response_op @ sky_op, d, output_dir_base,
-                                               title=title)
+    wgt_res = get_uncertainty_weighted_measure(sl, response_op @ sky_op, mask_op(d), output_dir_base,
+                                               mask_op=mask_op, title=title)
     return wgt_res
 
 
@@ -70,15 +75,18 @@ def weighted_residual_distribution(sl_path_base,
                                    data_path,
                                    sky_op,
                                    response_op,
+                                   mask_op,
                                    bins=20,
                                    output_dir_base=None,
                                    title='Weighted data residuals'):
     sl = ift.ResidualSampleList.load(sl_path_base)
     with open(data_path, "rb") as f:
         d = pickle.load(f)
-    wgt_res = get_uncertainty_weighted_measure(sl, response_op @ sky_op, d, None,
-                                               title=title)
+    wgt_res = get_uncertainty_weighted_measure(sl, response_op @ sky_op, mask_op(d), output_dir_base=None,
+                                               mask_op=mask_op, title=title)
     res = wgt_res.val.reshape(-1)
-    plt.histogram(res, bins)
-    plt.savefig(fname=title)
-    return
+    _, edges = np.histogram(res, bins=1000)
+    plt.hist(res, edges[1:])
+    plt.title(title)
+    plt.savefig(fname=output_dir_base+'.png')
+    return wgt_res
