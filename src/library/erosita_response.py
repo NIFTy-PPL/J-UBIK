62b5816466c668fa165d79fc37bb239a9dcde2dd
matteani <matteani@mpa-garching.mpg.de>
2023-08-22 13:39:45 +0200

    jubix_erosita_response: added readout application and small refactoring.

--- src/library/erosita_response.py
+++ src/library/erosita_response.py
@@ -1,151 +1,145 @@
-import math
-import os
-import pickle
-
-import nifty8 as ift
+import jax.numpy as jnp
 import numpy as np
 
-from .erosita_observation import ErositaObservation
-from .erosita_psf import eROSITA_PSF
-from .sky_models import SkyModel
-from .utils import get_config, create_output_directory, get_gaussian_psf, get_fft_psf_op, \
-    get_mask_operator
+
+def apply_erosita_exposure(exposures, exposure_cut=None):
+    """
+    Returns a function that applies eROSITA exposures to an input array.
+
+    Parameters
+    ----------
+    exposures: ndarray
+    Array with eROSITA exposure maps. The 0-th axis indexes the telescope module.
+    exposure_cut: float or None, optional
+        A threshold exposure value below which exposures are set to zero.
+        If None (default), no threshold is applied.
+
+    Returns
+    -------
+    callable
+        A function that takes an input array `x` and returns the element-wise
+        product of `exposures` and `x`, with the first dimension of `exposures`
+        broadcasted to match the shape of `x`.
+
+    Raises
+    ------
+    ValueError
+        If `exposures` is not a 2D array or `exposure_cut` is negative.
+    """
+    if exposure_cut < 0:
+        raise ValueError("exposure_cut should be positive!")
+    if exposure_cut is not None:
+        exposures[exposures < exposure_cut] = 0
+    return lambda x: exposures * x[jnp.newaxis, ...]
 
 
-def load_erosita_response(config_filepath, diagnostics_directory):
-    cfg = get_config(config_filepath)
-    fov = cfg['telescope']['fov']
-    rebin = math.floor(20 * fov // cfg['grid']['npix'])  # FIXME USE DISTANCES!
-    mock_run = cfg['mock']
-    mock_psf = cfg['mock_psf']
+def apply_erosita_exposure_from_file(exposure_filenames, exposure_cut=None):
+    """
+    Returns a function that applies eROSITA exposures loaded from files to an input array.
 
-    # Grid Info
-    grid_info = cfg['grid']
-    e_min = grid_info['energy_bin']['e_min']
-    e_max = grid_info['energy_bin']['e_max']
-    npix = grid_info['npix']
+    Parameters
+    ----------
+    exposure_filenames : list[str]
+        A list of file names containing eROSITA exposure maps in numpy format.
+    exposure_cut : float or None, optional
+        A threshold exposure value below which exposures are set to zero.
+        If None (default), no threshold is applied.
 
-    # Telescope Info
-    tel_info = cfg['telescope']
-    tm_ids = tel_info['tm_ids']
-    start_center = None
+    Returns
+    -------
+    callable
+        A function that takes an input array `x` and returns the element-wise
+        product of `exposures` and `x`, with the first dimension of `exposures`
+        broadcasted to match the shape of `x`.
 
-    # Exposure Info
-    det_map = tel_info['detmap']
+    Raises
+    ------
+    ValueError
+        If any of the exposure files cannot be loaded or `exposure_cut` is negative.
+    """
+    exposures = []
+    for file in exposure_filenames:
+        exposures.append(np.load(file))
+    exposures = np.array(exposures)
+    return apply_erosita_exposure(exposures, exposure_cut)
 
-    # Load file location
-    file_info = cfg['files']
-    obs_path = file_info['obs_path']
-    input_filenames = file_info['input']
-    sky_model = SkyModel(config_filepath)
-    sky_dict = sky_model.create_sky_model()
 
-    log = 'Output file {} already exists and is not regenerated. ' \
-          'If the observations parameters shall be changed please delete or rename the current ' \
-          'output file.'
+def apply_erosita_psf(shape, tm_ids, energy, center, convolution_method):
+    pass  # FIXME: implement
 
-    response_dict = {}
-    for tm_id in tm_ids:
-        tm_directory = create_output_directory(os.path.join(diagnostics_directory, f'tm{tm_id}'))
-        output_filename = f'{tm_id}_' + file_info['output']
-        exposure_filename = f'{tm_id}_' + file_info['exposure']
-        observation_instance = ErositaObservation(input_filenames, output_filename, obs_path)
-        # Create subdictionary to store individual tm response
-        tm_key = f'tm_{tm_id}'
-        response_dict[tm_key] = {}
-        response_subdict = response_dict[tm_key]
 
-        if not mock_run:
-            if not os.path.exists(os.path.join(obs_path, output_filename)):
-                observation_instance.get_data(emin=e_min, emax=e_max, image=True,
-                                              rebin=rebin,
-                                              size=npix, pattern=tel_info['pattern'],
-                                              telid=tm_id)  # FIXME: exchange rebin
-                # by fov? 80 = 4arcsec
-            else:
-                print(log.format(os.path.join(obs_path, output_filename)))
+def apply_erosita_psf_from_file():
+    pass  # FIXME: implement
 
-            observation_instance = ErositaObservation(output_filename, output_filename, obs_path)
 
-        # Exposure
-        if not os.path.exists(os.path.join(obs_path, exposure_filename)):
-            observation_instance.get_exposure_maps(output_filename, e_min, e_max,
-                                                   singlemaps=exposure_filename,
-                                                   withdetmaps=det_map)
+# def apply_erosita_response(x, fov, emin, emax, npix, tm_ids):
+#
+#
+#     R = mask @ sky_model.pad.adjoint @ exposure_op @ conv_op
+#     return R
 
-        else:
-            print(log.format(os.path.join(obs_path, output_filename)))
+def apply_erosita_readout(exposures, exposure_cut, tm_ids):
+    """
+    Applies a readout corresponding to the exposure masks.
 
-        # Plotting
-        plot_info = cfg['plotting']
-        if plot_info['enabled']:
-            if not mock_run:
-                observation_instance.plot_fits_data(output_filename,
-                                                    os.path.splitext(output_filename)[0],
-                                                    slice=plot_info['slice'],
-                                                    dpi=plot_info['dpi'])
-            observation_instance.plot_fits_data(exposure_filename,
-                                                f'{os.path.splitext(exposure_filename)[0]}.png',
-                                                slice=plot_info['slice'],
-                                                dpi=plot_info['dpi'])
+    Parameters
+    ----------
+        exposures : ndarray
+        Array with eROSITA exposure maps. The 0-th axis indexes the telescope module.
+        exposure_cut: float or None, optional
+            A threshold exposure value below which exposures are set to zero.
+            If None (default), no threshold is applied.
+        tm_ids : tuple or list
+            A tuple containing the ids of the telescope modules to be used as keys for the
+            response output dictionary
+    Returns
+    -------
+        function: A lambda function that extracts data from the exposures array
+            based on the tm_ids values.
+    Raises:
+    -------
+        ValueError: If exposure_cut is negative.
+    """
+    if exposure_cut < 0:
+        raise ValueError("exposure_cut should be positive!")
+    if exposure_cut is not None:
+        exposures[exposures < exposure_cut] = 0
+    mask = exposures == 0
+    return lambda x: {key: x[i][~mask[i]] for i, key in enumerate(tm_ids)}
 
-        # PSF
-        psf_filename = cfg['files']['psf_path'] + f'tm{tm_id}_' + cfg['files']['psf_base_filename']
-        if cfg['psf']['method'] in ['MSC', 'LIN']:
-            center_stats = observation_instance.get_pointing_coordinates_stats(tm_id,
-                                                                               input_filename=input_filenames)
-            print(center_stats['ROLL'])
-            center = (center_stats['RA'][0], center_stats['DEC'][0])
-            psf_file = eROSITA_PSF(psf_filename)
-            if start_center is None:
-                dcenter = (0, 0)
-                start_center = center
-            else:
-                dcenter = (cc - ss for cc, ss in zip(center, start_center))
 
-            dom = sky_model.position_space
-            center = tuple(0.5 * ss * dd for ss, dd in zip(dom.shape, dom.distances))
-            center = tuple(cc + dd for cc, dd in zip(center, dcenter))
+def apply_erosita_readout_from_file(exposure_filenames, exposure_cut, tm_ids):
+    """
+    Applies a readout corresponding to the exposure masks from file.
 
-            energy = cfg['psf']['energy']
-            conv_op = psf_file.make_psf_op(energy, center, sky_model.extended_space,
-                                           conv_method=cfg['psf']['method'],
-                                           conv_params=cfg['psf'])
+    Parameters
+    ----------
+        exposure_filenames : ndarray
+        A list of file names containing eROSITA exposure maps in numpy format.
+        exposure_cut: float or None, optional
+            A threshold exposure value below which exposures are set to zero.
+            If None (default), no threshold is applied.
+        tm_ids : tuple or list
+            A tuple containing the ids of the telescope modules to be used as keys for the
+            response output dictionary
+    Returns
+    -------
+        function: A lambda function that extracts data from the exposures array
+            based on the tm_ids values.
+    Raises:
+    -------
+        ValueError: If exposure_cut is negative.
+    """
+    exposures = []
+    for file in exposure_filenames:
+        exposures.append(np.load(file))
+    exposures = np.array(exposures)
+    return apply_erosita_readout(exposures, exposure_cut, tm_ids)
 
-        elif cfg['psf']['method'] == 'invariant':
-            if mock_psf:
-                conv_op = get_gaussian_psf(sky_dict['sky'], var=cfg['psf']['gauss_var'])
-            else:
-                center = observation_instance.get_pointing_coordinates_stats(tm_id)
-                psf_file = eROSITA_PSF(psf_filename)
-                psf_function = psf_file.psf_func_on_domain('3000', center, sky_model.extended_space)
-                psf_kernel = psf_function(*center)
-                psf_kernel = ift.makeField(sky_model.extended_space, np.array(psf_kernel))
-                conv_op = get_fft_psf_op(psf_kernel, sky_dict['sky'])
-        else:
-            raise NotImplementedError
-        response_subdict[f'convolution_op'] = conv_op
 
-        # Exposure
-        exposure = observation_instance.load_fits_data(exposure_filename)[0].data
-        exposure_cut = tel_info["exp_cut"]
-        if exposure_cut is not None:
-            exposure[exposure < exposure_cut] = 0
-        exposure_field = ift.makeField(sky_model.position_space, exposure)
+def apply_erosita_response_from_config():
+    pass
 
-        with open(tm_directory + f"/tm{tm_id}_exposure.pkl", "wb") as f:
-            pickle.dump(exposure_field, f)
-        padded_exposure_field = sky_model.pad(exposure_field)
-        exposure_op = ift.makeOp(padded_exposure_field)
-        response_subdict[f'exposure_field'] = exposure_field
-        response_subdict[f'exposure_op'] = exposure_op
 
-        # Mask
-        mask = get_mask_operator(exposure_field)
-        response_subdict[f'mask'] = mask
-
-        # Response
-        R = mask @ sky_model.pad.adjoint @ exposure_op @ conv_op
-        response_subdict[f'R'] = R
-
-    return response_dict
+def load_erosita_response():
+    pass
