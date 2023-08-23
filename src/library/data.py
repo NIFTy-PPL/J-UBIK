5aa90545e341c546bc54639fe90ef318998d9816
wmarg <margret@mpa-garching.mpg.de>
2023-08-23 11:55:59 +0200

    Changed to array and added test

--- src/library/data.py
+++ src/library/data.py
@@ -12,14 +12,43 @@ from .utils import get_config
 
 
 # GENERIC
-def load_masked_data_from_file(file_path, mask):
+def load_masked_data_from_pickle(file_path, mask_func):
+    """ Load data from pickle file as a data-dictionary and create a jft.Vector
+    of masked data out of it
+
+    Parameters
+    ----------
+    file_path : string
+        Path to data file (.pkl)
+    mask_func : Callable
+        Mask function, which takes a three dimensional array and makes a jft.Vector
+        out of it containing a dictionary of masked arrays
+    Returns
+    -------
+    masked_data : jft.Vector
+        Dictionary of masked data
+    """
     with open(file_path, "rb") as f:
         data_dict = pickle.load(f)
-    return jft.Vector({key: mask(value) for key, value in data_dict.items()})
+    masked_data_dict = np.stack(list(data_dict.values()))
+    if set(data_dict.keys()) != set(masked_data_dict):
+        raise ValueError('The loaded data dictionary and the given mask function '
+                         'are not compatible!')
+    return masked_data_dict
 
 
-def save_data_dict_to_file(data_dict, file_path):
-    #FIXME: Format check
+def save_data_dict_to_pickle(data_dict, file_path):
+    """ Save data dictionary to pickle file
+
+    Parameters
+    ----------
+    data_dict : dict
+        Data dictionary, which is saved.
+    file_path : string
+        Path to data file (.pkl)
+    Returns
+    -------
+    """
     with open(file_path, "wb") as file:
         pickle.dump(data_dict, file)
 
@@ -27,6 +56,29 @@ def save_data_dict_to_file(data_dict, file_path):
 # MOCK
 def generate_mock_sky_from_prior_dict(npix, padding_ratio, fov, priors, seed=42,
                                       default_point_source_prior=None):
+    """ Generates a mock sky position for the given grid and prior information
+
+    Parameters
+    ----------
+    npix : int
+        Number of pixels
+    padding_ratio : float
+        Ratio between padded and actual space
+    fov : int
+        FOV of the telescope
+    priors : dict
+        Dictionary of prior information
+    seed : int
+        Random seed for mock sky generation
+    default_point_source_prior: dict
+        Default values for the point sources for the mock sky generation if a reconstruction
+        with only diffuse is used.
+
+    Returns
+    -------
+    mock_sky_position: jft.Vector
+        Random position of the latent parameters
+    """
     if priors['point_sources'] is None and default_point_source_prior is None:
         raise ValueError('Point source information is needed for the generation of a mock sky.')
     if priors['point_sources'] is None:
@@ -38,18 +90,57 @@ def generate_mock_sky_from_prior_dict(npix, padding_ratio, fov, priors, seed=42,
 
 
 # eROSITA
-def load_erosita_masked_data(file_info, tel_info, mask):
+def load_erosita_masked_data(file_info, tel_info, mask_func):
+    """ Load eROSITA data from file, mask it and generate a jft.Vector out of it
+
+    Parameters
+    ----------
+    file_info : dict
+        Dictionary of file paths
+    tel_info : dict
+        Dictionary of telescope information
+    mask_func : Callable
+        Mask function, which takes a three dimensional array and makes a jft.Vector
+        out of it containing a dictionary of masked arrays
+    priors : dict
+        Dictionary of prior information
+    seed : int
+        Random seed for mock sky generation
+    default_point_source_prior: dict
+        Default values for the point sources for the mock sky generation if a reconstruction
+        with only diffuse is used.
+    Returns
+    -------
+    masked_data_vector: jft.Vector
+        Dictionary of masked data arrays
+    """
     data_list = []
     for tm_id in tel_info['tm_ids']:
         output_filename = f'{tm_id}_' + file_info['output']
         observation_instance = ErositaObservation(file_info['input'], output_filename, file_info['obs_path'])
         data = np.array(observation_instance.load_fits_data(output_filename)[0].data, dtype=int)
         data_list.append(data)
-    masked_data_vector = jft.Vector(mask(np.stack(data_list)))
+    masked_data_vector = mask_func(np.stack(data_list))
     return masked_data_vector
 
 
-def generate_erosita_data_from_config(config_file_path, response, output_path=None):
+def generate_erosita_data_from_config(config_file_path, response_func, output_path=None):
+    """ Generated mock data for the information given in the config_file and a response func
+
+    Parameters
+    ----------
+    config_file_path : string
+        Path to config
+    response_func : Callable
+        Response function, which takes an array and makes a jft.Vector
+        out of it containing a dictionary of mock data
+    output_path: string
+        If output_path is given the generated data is saved to a pickle file
+    Returns
+    -------
+    masked_mock_data: jft.Vector
+        Dictionary of masked mock data arrays
+    """
     cfg = get_config(config_file_path)
 
     tel_info = cfg['telescope']
@@ -64,8 +155,8 @@ def generate_erosita_data_from_config(config_file_path, response, output_path=No
                                                           cfg['point_source_defaults'])
     sky = xu.create_sky_model(grid_info['npix'], grid_info['padding_ratio'],
                               tel_info['fov'], priors)['sky']
-    masked_mock_data = response(sky(mock_sky_position))
+    masked_mock_data = response_func(sky(mock_sky_position))
     if output_path is not None:
-        save_data_dict_to_file(masked_mock_data, output_path)
+        save_data_dict_to_pickle(masked_mock_data.tree, output_path)
     return masked_mock_data
 
--- test/test_get_data.py
+++ test/test_get_data.py
@@ -1,35 +1,40 @@
 from jax import config
 from jax import random
 import numpy as np
+import unittest
 import xubik0 as xu
 
-config.update('jax_enable_x64', True)
-#TODO turn into pytest compatible test function
-if __name__ == "__main__":
-    seed = 42
-    key = random.PRNGKey(seed)
-    key, subkey = random.split(key)
-    config_file_path = 'config_test_sky_model.yaml'
-    cfg = xu.get_config(config_file_path)
 
-    file_info = cfg['files']
-    priors = cfg['priors']
-    grid_info = cfg['grid']
-    tel_info = cfg['telescope']
+class TestLoadData(unittest.TestCase):
 
-    keys = tel_info['tm_ids']
-    exposures = np.random.uniform(0.,
-                                  3e3,
-                                  size=len(keys)*grid_info['npix']**2).reshape(len(keys),
-                                                                    grid_info['npix'],
-                                                                    grid_info['npix'])
-    apply_exposure = xu.apply_exposure(exposures, 500)
-    mock_data = xu.generate_erosita_data_from_config(config_file_path,
-                                                     apply_exposure)
+    def test_load_and_generate_data(self):
+        config_file_path = 'config_test_sky_model.yaml'
+        cfg = xu.get_config(config_file_path)
 
-    apply_mask = xu.apply_exposure_readout(exposures, 500, keys)
-    for _, val in apply_mask(mock_data).items():
-        print(val.shape)
+        file_info = cfg['files']
+        grid_info = cfg['grid']
+        tel_info = cfg['telescope']
+        keys = tel_info['tm_ids']
+        exposures = np.random.uniform(0.,
+                                      3e3,
+                                      size=len(keys) * grid_info['npix'] ** 2).reshape(len(keys),
+                                                                                       grid_info['npix'],
+                                                                                       grid_info['npix'])
 
-    masked_data = xu.load_erosita_masked_data(file_info, tel_info, apply_mask)
-    print(masked_data)
+        apply_exposure = xu.apply_exposure(exposures, 500)
+        apply_mask = xu.apply_exposure_readout(exposures, 500, keys)
+        apply_response = lambda x: apply_mask(apply_exposure(x))
+        mock_data = xu.generate_erosita_data_from_config(config_file_path,
+                                                         apply_response)
+        masked_data = xu.load_erosita_masked_data(file_info, tel_info, apply_mask)
+
+        self.assertEqual(set(mock_data.tree.keys()), set(masked_data.tree.keys()))
+        for key in mock_data.tree.keys():
+            self.assertEqual(mock_data.tree[key].shape, masked_data.tree[key].shape)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
