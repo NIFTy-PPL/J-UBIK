ab52b0d68e17067a1947508215f96ea487cca397
Vincent Eberle <veberle@mpa-garching.mpg.de>
2023-08-23 13:32:57 +0200

    first test for patch conv

--- test/test_patch_conv.py
+++ test/test_patch_conv.py
@@ -1,19 +1,42 @@
-import jax
+import nifty8 as ift
 import numpy as np
 import xubik0 as xu
+
 import matplotlib.pyplot as plt
 
-# TODO Calculate Valid Shapes
-shape = (64, 64)
-n_patches = 4
-overlap = 0
+# load Chandra PSFS from file
+psf_array = np.load("../../perseus/processed_data/11713.npy",
+                    allow_pickle=True).item()["psf_sim"][0]
+psfs = np.array([p.val for p in psf_array])
 
-x = np.ones(shape)
-kernel = np.ones(shape)
-oa, padded = xu.linpatch_convolve(x, shape, kernel, n_patches, overlap)
-res = oa(padded)[0]
+# define domain
+shape = [512, 512]
+domain = ift.RGSpace(shape)
+test_f = ift.from_random(domain)
 
-print(res.shape)
-plt.imshow(res)
+# common parameters
+n_patches = 64
+n_patches_per_axis = int(np.sqrt(n_patches))
+margin = 10
+
+# nifty implementation
+old_conv = xu.OAnew.cut_force(domain, psfs, n_patches, margin, False)
+res1 = old_conv(test_f)
+
+# plot nifty result
+plt.imshow(res1.val)
 plt.colorbar()
 plt.show()
+
+plt.clf()
+
+# jax result
+psfs_prep = np.load("debugging_kernel.npy", allow_pickle=True).item().val
+res2 = xu.linpatch_convolve(test_f.val, shape, psfs_prep,
+                            n_patches_per_axis, margin)
+cres = res2 * domain.scalar_dvol
+
+plt.imshow(cres)
+plt.colorbar()
+plt.show()
+print("Results of two methods coincide:", np.allclose(cres, res1.val))
