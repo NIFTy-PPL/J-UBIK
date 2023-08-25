7e05d97bdbb10ee6a7f9fbf24d80c16e3a956b94
Vincent Eberle <veberle@mpa-garching.mpg.de>
2023-08-25 14:42:33 +0200

    pytest for patch_convolve
    compares to the old implementation

--- test/test_patch_conv.py
+++ test/test_patch_conv.py
@@ -2,41 +2,33 @@ import nifty8 as ift
 import numpy as np
 import xubik0 as xu
 
+import pytest
 import matplotlib.pyplot as plt
 
-# load Chandra PSFS from file
-psf_array = np.load("../../perseus/processed_data/11713.npy",
-                    allow_pickle=True).item()["psf_sim"][0]
-psfs = np.array([p.val for p in psf_array])
 
-# define domain
-shape = [512, 512]
-domain = ift.RGSpace(shape)
-test_f = ift.from_random(domain)
+def test_lin_patch_conv():
+    # load Chandra PSFS from file
+    psf_array = np.load("../../perseus/processed_data/11713.npy",
+                        allow_pickle=True).item()["psf_sim"][0]
+    psfs = np.array([p.val for p in psf_array])
 
-# common parameters
-n_patches = 64
-n_patches_per_axis = int(np.sqrt(n_patches))
-margin = 10
+    # define domain
+    shape = [512, 512]
+    domain = ift.RGSpace(shape)
+    test_f = ift.from_random(domain)
 
-# nifty implementation
-old_conv = xu.OAnew.cut_force(domain, psfs, n_patches, margin, False)
-res1 = old_conv(test_f)
+    # common parameters
+    n_patches = 64
+    n_patches_per_axis = int(np.sqrt(n_patches))
+    margin = 10
 
-# plot nifty result
-plt.imshow(res1.val)
-plt.colorbar()
-plt.show()
+    # nifty result
+    old_conv = xu.OAnew.cut_force(domain, psfs, n_patches, margin, False)
+    res1 = old_conv(test_f)
 
-plt.clf()
+    # jax result
+    psfs_prep = np.load("debugging_kernel.npy", allow_pickle=True).item().val
+    res2 = xu.linpatch_convolve(test_f.val, domain, psfs_prep,
+                                n_patches_per_axis, margin)
 
-# jax result
-psfs_prep = np.load("debugging_kernel.npy", allow_pickle=True).item().val
-res2 = xu.linpatch_convolve(test_f.val, shape, psfs_prep,
-                            n_patches_per_axis, margin)
-cres = res2 * domain.scalar_dvol
-
-plt.imshow(cres)
-plt.colorbar()
-plt.show()
-print("Results of two methods coincide:", np.allclose(cres, res1.val))
+    np.testing.assert_allclose(res2, res1.val)
