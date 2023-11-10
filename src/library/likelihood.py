911216dc72facc9ce56e34c71200e4f3b9aa1ed4
matteani <matteani@mpa-garching.mpg.de>
2023-11-10 10:41:51 +0100

    97-refactor-jubix-response: refactored the response creation from `likelihood.py` to `response.py`.

--- src/library/likelihood.py
+++ src/library/likelihood.py
@@ -1,12 +1,11 @@
 import os
+
 import nifty8.re as jft
-import xubik0 as xu
-import numpy as np
-from .data import (load_erosita_masked_data, generate_erosita_data_from_config,
-                   load_masked_data_from_pickle, Domain)
-from .response import (build_callable_from_exposure_file, build_erosita_psf,
-                       build_readout_function, build_exposure_function)
-from .erosita_observation import ErositaObservation
+
+from .data import load_erosita_masked_data, generate_erosita_data_from_config, \
+    load_masked_data_from_pickle
+from .response import build_erosita_response_from_config
+from .utils import get_config
 
 
 def generate_erosita_likelihood_from_config(config_file_path):
@@ -23,49 +22,14 @@ def generate_erosita_likelihood_from_config(config_file_path):
     """
 
     # load config
-    cfg = xu.get_config(config_file_path)
-    tel_info = cfg['telescope']
+    cfg = get_config(config_file_path)
     file_info = cfg['files']
-    psf_info = cfg['psf']
-    # lists for exposure and psf files
-    exposure_file_names = [os.path.join(file_info['obs_path'], f'{key}_'+file_info['exposure'])
-                           for key in tel_info['tm_ids']]
-    psf_file_names = [os.path.join(file_info['psf_path'], 'tm'+f'{key}_'+file_info['psf_base_filename'])
-                      for key in tel_info['tm_ids']]
+    tel_info = cfg['telescope']
 
-    # Get pointings for different telescope modules in RA/DEC
-    obs_instance = ErositaObservation(file_info['input'],
-                                      file_info['output'],
-                                      file_info['obs_path'])
-    center_stats = []
-    for tm_id in tel_info['tm_ids']:
-        tmp_center_stat = obs_instance.get_pointing_coordinates_stats(tm_id, file_info['input'])
-        tmp_center_stat = [tmp_center_stat['RA'][0], tmp_center_stat['DEC'][0]]
-        center_stats.append(tmp_center_stat)
-    center_stats = np.array(center_stats)
-    # with respect to TM1
-    ref_center = center_stats[0]
-    d_centers = center_stats - ref_center
-    # Set the Image pointing to the center and associate with TM1 pointing
-    image_pointing_center = np.array(tuple([cfg['telescope']['fov']/2.]*2))
-    pointing_center = d_centers + image_pointing_center
-    domain = Domain(tuple([cfg['grid']['npix']]*2), tuple([cfg['telescope']['fov']/cfg['grid']['npix']]*2))
+    response_dict = build_erosita_response_from_config(config_file_path)
 
-    # get psf/exposure/mask function
-    psf_func = build_erosita_psf(psf_file_names, psf_info['energy'], pointing_center, domain,
-                                 psf_info['npatch'], psf_info['margfrac'], psf_info['want_cut'],
-                                 psf_info['method'])
-
-    exposure_func = build_callable_from_exposure_file(build_exposure_function,
-                                                      exposure_file_names,
-                                                      exposure_cut=tel_info['exp_cut'])
-
-    mask_func = build_callable_from_exposure_file(build_readout_function,
-                                                  exposure_file_names,
-                                                  threshold=tel_info['exp_cut'],
-                                                  keys=tel_info['tm_ids'])
-    # plugin
-    response_func = lambda x: mask_func(exposure_func(psf_func(x))[:,43:-43,43:-43])
+    response_func = response_dict['R']
+    mask_func = response_dict['mask']
 
     if cfg['mock']:
         masked_data = generate_erosita_data_from_config(config_file_path, response_func,
