a8b4ddf497fb5a802fce9fe0693d26ff613396c7
Vincent Eberle <veberle@mpa-garching.mpg.de>
2024-04-08 14:41:32 +0200

    final MF sky model

--- src/library/mf_sky.py
+++ src/library/mf_sky.py
@@ -2,139 +2,119 @@
 
 from functools import partial, reduce
 import jax
+import jax.numpy as jnp
+
 import nifty8.re as jft
 
 
 class MappedModel(jft.Model):
-    def __init__(self, correlated_field, cf_prefix, shape, first_axis=True):
-        """
+    """Maps a model to a higher dimensional space."""
+
+    def __init__(self, model, mapped_key, shape, first_axis=True):
+        """Intitializes the mapping class.
+
         Parameters:
         ----------
-        correlated_field : CF Model
-        cf_prefix: probably string
-        shape: tuple, number of copies in each dim
-        first_axis: if True prepends the ndof copies els appends
+        model: nifty.re.Model most probable a Correlated Field Model or a
+            Gauss-Markov Process
+        mapped_key: string, dictionary key for input dimension which is
+            going to be mapped.
+        shape: tuple, number of copies in each dim. Size of the
+        first_axis: if True prepends the number of copies
+            else they will be appended
         """
-        self._cf = correlated_field
+        self._model = model
         ndof = reduce(lambda x, y: x * y, shape)
-        keys = correlated_field.domain.keys()
-        xi_key = cf_prefix + 'xi'
-        if xi_key not in keys:
+        keys = model.domain.keys()
+        if mapped_key not in keys:
             raise ValueError
 
-        xi_dom = correlated_field.domain[xi_key]
+        xi_dom = model.domain[mapped_key]
         if first_axis:
             new_primals = jft.ShapeWithDtype((ndof,) + xi_dom.shape, xi_dom.dtype)
             axs = 0
             self._out_axs = 0
-            self._shape = shape + xi_dom.shape
+            self._shape = shape + model.target.shape
         else:
             new_primals = jft.ShapeWithDtype(xi_dom.shape + (ndof,), xi_dom.dtype)
             axs = -1
             self._out_axs = 1
-            self._shape = xi_dom.shape + shape
-            
-        new_domain = correlated_field.domain.copy()
-        new_domain[xi_key] = new_primals
+            self._shape = model.target.shape + shape
+
+        new_domain = model.domain.copy()
+        new_domain[mapped_key] = new_primals
 
         xiinit = partial(jft.random_like, primals=new_primals)
 
-        init = correlated_field.init
-        init = {k: init[k] if k != xi_key else xiinit for k in keys}
+        init = model.init
+        init = {k: init[k] if k != mapped_key else xiinit for k in keys}
 
-        self._axs = ({k: axs if k == xi_key else None for k in keys},)
+        self._axs = ({k: axs if k == mapped_key else None for k in keys},)
         super().__init__(domain=new_domain, init=jft.Initializer(init))
 
     def __call__(self, x):
         x = x.tree if isinstance(x, jft.Vector) else x
-        return (jax.vmap(self._cf, in_axes=self._axs, out_axes=self._out_axs)(x)).reshape(
-            self._shape)
+        return (jax.vmap(self._model, in_axes=self._axs,
+                         out_axes=self._out_axs)(x)).reshape(self._shape)
 
 
-def mf_model(freqs, alph, spatial, dev):
-    """Typical Multifrequency Model for physical forward modelling.
+class GeneralModel(jft.Model):
+    """#FIXME Some Docstring."""
 
-    This models physical field which is correlated spatially and its
-    Energy follows a power law function with spectral indices alpha,
-    which can be a field (correlated or not, spatially) with and offset
-    (value of the spatial field) and deviations on the powerlaw (dev)
+    def __init__(self, dict_of_fields={}):
+        """Initziales the general sky model.
+        #NOTE Write some text, which of the keys are allowed.
 
-    # TODO for iregular gridded freqs, the correlated field model cannot be
-    used for the deviations
+        Paramters:
+        ----------
+        dict of fields: the respective keys and the
+            nifty.re.models as values"""
+        self._available_fields = dict_of_fields
 
-    """
+    def build_model(self):
+        """#NOTE Docstring."""
+        def add_functions(f1, f2):
+            def function(x):
+                return f1(x) + f2(x)
+            return function
+
+        if 'spatial' not in self._available_fields.keys() or self._available_fields['spatial'] is None:
+            raise NotImplementedError
+        else:
+            spatial = self._available_fields['spatial']
+            func = spatial
+            domain = spatial.domain
+            if 'freq_plaw' in self._available_fields.keys() and self._available_fields['freq_plaw'] is not None:
+                plaw = self._available_fields['freq_plaw']
+                func = add_functions(func, plaw)
+                domain = domain | plaw.domain
+            if 'freq_dev' in self._available_fields.keys() and self._available_fields['freq_dev'] is not None:
+                dev = self._available_fields['freq_dev']
+
+                def extract_keys(a, domain):
+                    b = {key: a[key] for key in domain}
+                    return b
+
+                def extracted_dev(op):
+                    def callable_dev(x):
+                        return op(extract_keys(x, op.domain))
+                    return callable_dev
+
+                func = add_functions(func, extracted_dev(dev))
+                domain = domain | dev.domain
+            if 'pol' in self._available_fields.keys() and self._available_fields['pol'] is not None:
+                raise NotImplementedError
+            if 'time' in self._available_fields.keys() and self._available_fields['time'] is not None:
+                raise NotImplementedError
+            res = jft.Model(func, domain=domain)
+        return res
+
+
+def build_power_law(freqs, alph):
+    """Models a power law. Building bloc for e.g. a multifrequency model"""
     if isinstance(alph, jft.Model):
-        plaw = lambda x: jnp.outer(freqs, alph(x)).reshape(freqs.shape + alph.target.shape)
-        plaw_offset = lambda x: plaw(x) + spatial(x)
-        def extract_keys(a, domain):
-            b = {key: a[key] for key in domain}
-            return b
-        res = lambda x: plaw_offset(x) + dev(extract_keys(x, dev.domain))
-        domain = alph.domain | spatial.domain | dev.domain
-        res = jft.Model(res, domain=domain)
+        res = lambda x: jnp.outer(freqs, alph(x)).reshape(freqs.shape + alph.target.shape)
     elif isinstance(alph, float):
+        # FIXME not working at the moment
         res = jnp.outer(freqs, alph).reshape(freqs.shape)
-    return res
-
-# import nifty8 as ift
-# from jax import random
-# import jax.numpy as jnp
-# import numpy as np
-# import jax
-# from nifty8.re.tree_math import ShapeWithDtype
-
-# # Random Stuff
-# seed = 42
-# key = random.PRNGKey(seed)
-# key, subkey = random.split(key)
-
-# dims = (10)
-
-# # Correlated Field Energy 1D
-# cf_zm = {"offset_mean": 0., "offset_std": (1e-3, 1e-4)}
-# cf_fl = {
-#     "fluctuations": (1e-1, 5e-3),
-#     "loglogavgslope": (-1., 1e-2),
-#     "flexibility": (1e+0, 5e-1),
-#     "asperity": (5e-1, 5e-2),
-#     "harmonic_type": "Fourier"
-# }
-# cfm = jft.CorrelatedFieldMaker("cf")
-# cfm.set_amplitude_total_offset(**cf_zm)
-# cfm.add_fluctuations(
-#     dims,
-#     distances=1. / dims,
-#     **cf_fl,
-#     prefix="ax1",
-#     non_parametric_kind="power"
-# )
-# correlated_field = cfm.finalize()
-
-# # Initial Position for 1D Field
-# pos_init = jft.Vector(jft.random_like(subkey, cfm._parameter_tree))
-# print(correlated_field(pos_init).shape)
-
-# # Partial dictionary Evaluation for enabling vmap
-# def partial_apply_cf(cf, pos_init):
-#     def partly(xi, xi_key):
-#         pos_init.tree.pop(xi_key)
-#         pos_init.tree.update({xi_key: xi})
-#         return cf(pos_init)
-#     return partly
-
-
-# func = partial_apply_cf(correlated_field, pos_init)
-
-# # Bigger parametertree
-# dict = cfm._parameter_tree # = domain for CF
-# dict.update({"cfxi": ShapeWithDtype(shape=(10, 200))})
-
-# # New position, but bigger
-# new_pos_init = jft.Vector(jft.random_like(subkey, dict))
-
-# # The magic line
-# cf_list = jax.vmap(func, in_axes=(1, None), out_axes=1)#(new_pos_init.tree['cfxi'], 'cfxi')
-# print(cf_list)
-# res = cf_list(new_pos_init.tree['cfxi'], 'cfxi')
-# jft.Model(cf_list, domain=dict)
-# print(res.shape)
+        return jft.Model(res, domain=alph.domain)
