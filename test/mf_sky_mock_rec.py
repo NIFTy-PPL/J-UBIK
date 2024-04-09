e2c66f63779d4099f353d83ff03aecb044171dde
wmarg <margret@mpa-garching.mpg.de>
2024-04-09 14:00:44 +0200

    Start of MF tests

--- test/mf_sky_mock_rec.py
+++ test/mf_sky_mock_rec.py
@@ -2,6 +2,9 @@ import argparse
 import os
 from jax import random
 import jax.numpy as jnp
+import numpy as np
+import nifty8 as ift
+import matplotlib.pyplot as plt
 
 import jubik0 as ju
 import nifty8.re as jft
@@ -49,7 +52,7 @@ def main():
     )
     alpha_field = alpha.finalize()
 
-    plaw = ju.build_power_law(jnp.array(cfg['energy']['freqs']), alpha_field)
+    plaw = ju.build_power_law(np.arange(0, e_dims, 1), alpha_field)
 
     # Spectral plaw dev
     dev_dict = cfg['energy']['priors']['plaw']
@@ -68,10 +71,20 @@ def main():
     final_model = jft.Model(final_exp, domain=gen_mod.domain)
 
     key = random.PRNGKey(seed)
-    key, subkey = random.split(key)
-    pos_init = jft.Vector(jft.random_like(subkey, final_model.domain))
-    mf_prior_field = final_model(pos_init)
+    for j in range(10):
+        key, subkey = random.split(key)
+        pos_init = jft.Vector(jft.random_like(subkey, final_model.domain))
+        mf_prior_field = final_model(pos_init)
+        plot = ift.Plot()
+        domain = ift.makeDomain((ift.RGSpace((e_dims)), ift.RGSpace(s_dims)))
+        plot.add(ift.Field.from_raw(domain, np.array(mf_prior_field)), freq_space_idx=0,
+                 map_energies_logarithmically=False)
+        plot.output(name=f'out{j}.png')
 
+    plt.plot(np.arange(0, e_dims, 1), mf_prior_field[:, 256, 256])
+    plt.show()
+    plt.close()
+    exit()
     lklhd = jft.Poissonian(mf_prior_field.astype(int)).amend(final_model)
 
     pos_init = 0.1 * jft.Vector(jft.random_like(subkey, gen_mod.domain))
