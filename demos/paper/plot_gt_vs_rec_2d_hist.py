e2208446634a099f2f677c37acd29686bcb988f0
wmarg <margret@mpa-garching.mpg.de>
2024-08-08 11:47:55 +0200

    Add simulated rec plotting

---
+++ demos/paper/plot_gt_vs_rec_2d_hist.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+from jax import linear_transpose, vmap
+import jax.numpy as jnp
+import jax
+
+import jubik0 as ju
+from os.path import join
+import astropy.io.fits as fits
+import nifty8.re as jft
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from plot_eROSITA_image import plot_2d_gt_vs_rec_histogram
+
+
+# Script for plotting the data, position and reconstruction images
+if __name__ == "__main__":
+    results_path = "results/LMC-06082024-002M-mock"
+    config_name = "eROSITA_config_small.yaml"
+    output_dir = ju.create_output_directory(join(results_path, 'paper'))
+    config_path = join(results_path, config_name)
+    config_dict = ju.get_config(config_path)
+    tm_ids = config_dict["telescope"]["tm_ids"]
+
+    with open(os.path.join(results_path, 'last.pkl'), "rb") as file:
+        samples, _ = pickle.load(file)
+
+    sky_model = ju.SkyModel(config_path)
+    sky = sky_model.create_sky_model()
+    sky_dict = sky_model.sky_model_to_dict()
+    response_dict = ju.build_erosita_response_from_config(config_path)
+
+    mask_adj = jax.linear_transpose(response_dict['mask'],
+                                    np.zeros((len(tm_ids),) + sky.target.shape))
+    response_dict['mask_adj'] = mask_adj
+    mask_func = response_dict['mask']
+
+    gt_dict = {}
+    masked_data = ju.load_masked_data_from_config(config_path)
+    pos = ju.load_mock_position_from_config(config_path)
+    for key, comp in sky_dict.items():
+        gt_dict[key] = comp(pos)
+
+    masked_data = jax.tree_map(lambda x: np.array(x, dtype=np.float64),
+                            masked_data)
+    plotting_kwargs =  {'bins': 600, 'x_label': '$s_{gt}$', 'y_label': 'a', 'dpi': 400, 'title': 'piiip'}
+    ju.plot_2d_gt_vs_rec_histogram(samples, sky_dict, output_dir, response_dict,
+                                gt_dict, plot_kwargs=plotting_kwargs, base_filename='gtvsrec')
---
+++ demos/paper/visualize_mock_sky.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+from jax import linear_transpose, vmap
+import jax.numpy as jnp
+
+import jubik0 as ju
+from os.path import join
+import astropy.io.fits as fits
+import nifty8.re as jft
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from plot_eROSITA_image import plot, plot_rgb
+
+
+# Script for plotting the data, position and reconstruction images
+if __name__ == "__main__":
+    results_path = "results/LMC-06082024-002M-mock"
+    config_name = "eROSITA_config_small.yaml"
+    output_dir = ju.create_output_directory(join(results_path, 'paper'))
+    config_path = join(results_path, config_name)
+    config_dict = ju.get_config(config_path)
+
+    pos = ju.load_mock_position_from_config(config_path)
+
+    sky_model = ju.SkyModel(config_path)
+    sky = sky_model.create_sky_model()
+    sky_dict = sky_model.sky_model_to_dict()
+
+    sat_max = {'sky': [2e-6, 2e-7, 5e-8], 'diffuse': [2e-6, 2e-7, 5e-8], 'points': [5e-8, 5e-8, 5e-8]}
+    for key, op in sky_dict.items():
+        real_pos = op(pos)
+        bbox_info = [(7, 4), 7, 24]
+        plot_rgb(real_pos, sat_min=[1e-9, 1e-9, 1e-9],
+                 sat_max=sat_max[key],
+                 sigma=None, log=True,
+                 title='simulated sky', fs=18, pixel_measure=28,
+                 output_file=join(output_dir, f'simulated_{key}.png'),
+                 alpha=0.5,
+                 bbox_info=bbox_info
+                 )
+
