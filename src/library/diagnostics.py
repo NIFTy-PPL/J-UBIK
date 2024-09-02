6f0ce96cc00cac20afb3bfc073c413ac9e98782c
wmarg <margret@mpa-garching.mpg.de>
2024-09-02 16:07:49 +0200

    Get rid of jax and numpy

--- src/library/diagnostics.py
+++ src/library/diagnostics.py
@@ -5,7 +5,7 @@ from jax import numpy as jnp
 
 def calculate_uwr(pos, op, ground_truth, response_dict,
                    abs=False, exposure_mask=True, log=True):
-    op = jax.vmap(op)
+    op = vmap(op)
     if log:
         ground_truth = jnp.log(ground_truth) if ground_truth is not None else 0.
         res = (jnp.mean(jnp.log(op(pos)), axis=0) - ground_truth) / jnp.std(jnp.log(op(pos)),
@@ -17,7 +17,7 @@ def calculate_uwr(pos, op, ground_truth, response_dict,
         res = jnp.abs(res)
     if exposure_mask:
         exposure = response_dict['exposure']
-        exposure_mask = np.array(exposure(np.ones_like(res))).sum(axis=0) > 0
+        exposure_mask = jnp.array(exposure(jnp.ones_like(res))).sum(axis=0) > 0
     return res, exposure_mask
 
 
@@ -31,25 +31,25 @@ def calculate_nwr(pos, op, data, response_dict,
     adj_mask = response_dict['mask_adj']
     sqrt = lambda x: tree_map(jnp.sqrt, x)
     res = lambda x: (R(op(x)) - data) / sqrt(R(op(x)))
-    res = jax.vmap(res, out_axes=1)
-    res = np.array(jax.vmap(adj_mask, in_axes=1, out_axes=1)(res(pos))[0])
+    res = vmap(res, out_axes=1)
+    res = jnp.array(vmap(adj_mask, in_axes=1, out_axes=1)(res(pos))[0])
     if abs:
-        res = np.abs(res)
+        res = jnp.abs(res)
 
     min_count_mask = None
     if min_counts is not None:
-        masked_indices = lambda x: np.array(x < min_counts, dtype=float)
+        masked_indices = lambda x: jnp.array(x < min_counts, dtype=float)
         masked_indices = tree_map(masked_indices, data)
         min_count_mask = lambda x: adj_mask(masked_indices)[0]
     if exposure_mask:
-        exp_mask = lambda x: response_dict['exposure'](np.ones(op(x).shape)) == 0.
+        exp_mask = lambda x: response_dict['exposure'](jnp.ones(op(x).shape)) == 0.
         if min_count_mask is not None:
-            tot_mask = lambda x: np.logical_or(min_count_mask(x), exp_mask(x), dtype=bool)
+            tot_mask = lambda x: jnp.logical_or(min_count_mask(x), exp_mask(x), dtype=bool)
         else:
             tot_mask = exp_mask
     else:
         tot_mask = min_count_mask if min_count_mask is not None else None
     if tot_mask is not None:
-        tot_mask = jax.vmap(tot_mask, out_axes=1)
+        tot_mask = vmap(tot_mask, out_axes=1)
     return res, tot_mask(pos)
 
