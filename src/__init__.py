79125beeae9257db93d6332afbc1d43a02e4012a
Vincent Eberle <veberle@mpa-garching.mpg.de>
2024-09-04 17:18:47 +0200

    move function

--- src/__init__.py
+++ src/__init__.py
@@ -11,8 +11,7 @@ from .library.utils import (save_to_pickle, load_from_pickle,
                             get_radec_from_xy, get_psfpatches,
                             get_synth_pointsource, get_gaussian_psf, get_rel_uncertainty,
                             get_equal_lh_transition, get_RGB_image_from_field, get_stats,
-                            save_local_packages_hashes_to_txt, safe_config_update,
-                            get_n_constrained_dof)
+                            save_local_packages_hashes_to_txt, safe_config_update)
 from .library.plot import (plot_result, plot_histograms,
                            plot_sample_averaged_log_2d_histogram, plot_rgb)
 from .library.sugar_plot import (plot_pspec, plot_sample_and_stats, plot_sample_and_stats,
@@ -33,7 +32,7 @@ from .library.instruments.erosita.erosita_data import create_erosita_data_from_c
 from .library.instruments.erosita.erosita_psf import eROSITA_PSF
 from .library.data import (create_mock_data, load_masked_data_from_config,
                            load_mock_position_from_config, Domain)
-from .library.likelihood import generate_erosita_likelihood_from_config
+from .library.likelihood import generate_erosita_likelihood_from_config, get_n_constrained_dof
 from .library.diagnostics import (calculate_nwr,
                                   calculate_uwr)
 from .library.jifty_convolution_operators import (_bilinear_weights,
--- src/library/likelihood.py
+++ src/library/likelihood.py
@@ -29,3 +29,27 @@ def generate_erosita_likelihood_from_config(config_file_path):
     masked_data = load_masked_data_from_config(config_file_path)
     response_func = response_dict['R']
     return jft.Poissonian(masked_data).amend(response_func)
+
+
+def get_n_constrained_dof(likelihood: jft.Likelihood) -> int:
+    """
+    Extacts the number of constrained degrees of freedom (DOF)
+    based on the likelihood.
+
+    Parameters
+    ----------
+    likelihood : jft.Likelihood
+        The likelihood object which contains information about
+       the model and data.
+
+    Returns
+    -------
+    int
+        The number of constrained degrees of freedom, which is the
+        minimum of the model degrees of freedom and the data
+        degrees of freedom.
+    """
+
+    n_dof_data = jft.size(likelihood.left_sqrt_metric_tangents_shape)
+    n_dof_model = jft.size(likelihood.domain)
+    return min(n_dof_model, n_dof_data)
