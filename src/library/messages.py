fa2f7b8528185fa9d221345580830f8b8012b18b
wmarg <margret@mpa-garching.mpg.de>
2024-09-05 15:07:51 +0200

    Check indents

--- src/library/messages.py
+++ src/library/messages.py
@@ -35,30 +35,14 @@ def message_obs(obsInfo, verbose=1):
         print('Observation Nr. {:d} Information:'.format(obsInfo['obsID']))
         print('------------------------')
         print('Nominal pointing: RA = {:.4f}, DEC = {:.4f}, ROLL = {:.4f} \
-        degrees'.format(obsInfo['ra'], obsInfo['dec'], obsInfo['roll']))
+            degrees'.format(obsInfo['ra'], obsInfo['dec'], obsInfo['roll']))
         print('Observation Duration = {:.4f} s'.format(obsInfo['duration']))
         print('')
         print('Considered range in detector coordinates x = {0:.1f} - {1:.1f},\
-         y = {2:.1f} - {3:.1f} pixels'.format(obsInfo['x_min'],
+            y = {2:.1f} - {3:.1f} pixels'.format(obsInfo['x_min'],
                                               obsInfo['x_max'],
                                               obsInfo['y_min'],
                                               obsInfo['y_max']))
-        print('Considered energy range = {0:.1f} to {1:.1f} keV'.format(
-            obsInfo['energy_min'], obsInfo['energy_max']))
-        print('------------------------')
-    if verbose > 0:
-
-        print('')
-        print('Observation Nr. {:d} Information:'.format(obsInfo['obsID']))
-        print('------------------------')
-        print('Nominal pointing: RA = {:.4f}, DEC = {:.4f}, ROLL = {:.4f} \
-        degrees'.format(obsInfo['ra'], obsInfo['dec'], obsInfo['roll']))
-        print('Observation Duration = {:.4f} s'.format(obsInfo['duration']))
-        print('')
-
-        print('Considered range in detector coordinates x = {0:.1f} - {1:.1f},\
-        y = {2:.1f} - {3:.1f} pixels'.format(obsInfo['x_min'],\
-               obsInfo['x_max'], obsInfo['y_min'], obsInfo['y_max']))
         print('Considered energy range = {0:.1f} to {1:.1f} keV'.format(
             obsInfo['energy_min'], obsInfo['energy_max']))
         print('------------------------')
