04176c18a3b4516ba2442e57806679d469831fb6
wmarg <margret@mpa-garching.mpg.de>
2024-09-10 10:53:17 +0200

    Fix test sky model

--- test/library/test_sky_model.py
+++ test/library/test_sky_model.py
@@ -7,22 +7,72 @@ import jubik0 as ju
 import nifty8.re as jft
 
 @pytest.fixture
-def config():
-    return 'instruments/erosita/config_test_sky_model.yaml'
+def priors():
+    priors_dict = {}
+    priors_dict['diffuse'] = {}
+    priors_dict['diffuse']['spatial'] = {}
+    priors_dict['diffuse']['spatial']['offset'] = {}
+    priors_dict['diffuse']['spatial']['offset']['offset_mean'] = -14.0
+    priors_dict['diffuse']['spatial']['offset']['offset_std'] = [0.5, 0.05]
+
+    priors_dict['diffuse']['spatial']['fluctuations'] = {}
+    priors_dict['diffuse']['spatial']['fluctuations']['fluctuations'] = [0.5,
+                                                                       0.2]
+    priors_dict['diffuse']['spatial']['fluctuations']['loglogavgslope'] =\
+    [-4.0, 0.3]
+    priors_dict['diffuse']['spatial']['fluctuations']['flexibility'] = \
+    [0.4, 0.1]
+    priors_dict['diffuse']['spatial']['fluctuations']['asperity'] = None
+    priors_dict['diffuse']['spatial']['fluctuations']\
+        ['non_parametric_kind'] = 'power'
+    priors_dict['diffuse']['spatial']['prefix'] = 'diffuse_spatial_'
+
+    priors_dict['diffuse']['plaw'] = {}
+    priors_dict['diffuse']['plaw']['offset'] = {}
+    priors_dict['diffuse']['plaw']['offset']['offset_mean'] = -2.0
+    priors_dict['diffuse']['plaw']['offset']['offset_std'] = [0.3, 0.05]
+
+    priors_dict['diffuse']['plaw']['fluctuations'] = {}
+    priors_dict['diffuse']['plaw']['fluctuations']['fluctuations'] = [0.5,
+                                                                       0.2]
+    priors_dict['diffuse']['plaw']['fluctuations']['loglogavgslope'] =\
+    [-4.0, 0.3]
+    priors_dict['diffuse']['plaw']['fluctuations']['flexibility'] = \
+    [0.4, 0.1]
+    priors_dict['diffuse']['plaw']['fluctuations']['asperity'] = None
+    priors_dict['diffuse']['plaw']['fluctuations']\
+        ['non_parametric_kind'] = 'power'
+    priors_dict['diffuse']['plaw']['prefix'] = 'diffuse_plaw_'
+
+    return priors_dict
 
 @pytest.fixture
-def sky_model(config):
-    return ju.SkyModel(config)
+def config(priors):
+    cfg_dict = {}
+    cfg_dict['sdim'] = 128
+    cfg_dict['edim'] = 3
+    cfg_dict['s_padding_ratio'] = 1.1
+    cfg_dict['e_padding_ratio'] = 1.0
+    cfg_dict['fov'] = 4096
+    cfg_dict['e_min'] = [0.2, 1.0, 2.0]
+    cfg_dict['e_max'] = [1.0, 2.0, 4.0]
+    cfg_dict['e_ref'] = 2.0
+    cfg_dict['priors'] = priors
+    return cfg_dict
 
-def test_sky_model_creation(sky_model):
+@pytest.fixture
+def sky_model():
+    return ju.SkyModel()
+
+def test_sky_model_creation(sky_model, config):
     assert sky_model is not None
-    sky = sky_model.create_sky_model()
+    sky = sky_model.create_sky_model(**config)
     assert isinstance(sky, jft.Model)
     sky_dict = sky_model.sky_model_to_dict()
     assert 'sky' in sky_dict
 
-def test_sky_application(sky_model):
-    sky = sky_model.create_sky_model()
+def test_sky_application(sky_model, config):
+    sky = sky_model.create_sky_model(**config)
     key = random.PRNGKey(81)
     key, subkey = random.split(key)
 
@@ -30,3 +80,4 @@ def test_sky_application(sky_model):
 
     sky_real = sky(pos)
     assert isinstance(sky_real, jnp.ndarray)
+
