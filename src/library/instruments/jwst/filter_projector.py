1fc2260081f082b5b1e5c434e3aafb3bfac9532f
matteani <matteani@mpa-garching.mpg.de>
2024-09-11 11:08:30 +0200

    joss_paper_jwst: filter_projector.py documentation

--- src/library/instruments/jwst/filter_projector.py
+++ src/library/instruments/jwst/filter_projector.py
@@ -2,7 +2,27 @@ import nifty8.re as jft
 
 
 class FilterProjector(jft.Model):
+    """
+    A nifty8.re.Model that projects input data into specified filters
+    defined by color keys.
+
+    The FilterProjector class takes a sky domain and a mapping between keys
+    and colors, and applies a projection of input data according to the filters.
+    It supports querying keys based on colors and efficiently applies
+    transformations for multi-channel inputs.
+    """
     def __init__(self, sky_domain: jft.ShapeWithDtype, keys_and_colors: dict):
+        """
+        Parameters
+        ----------
+        sky_domain : jft.ShapeWithDtype
+            The domain for the sky data, defining the shape and data type of
+            the input.
+        keys_and_colors : dict
+            A dictionary where the keys are filter names (or keys) and the
+            values are lists of colors associated with each filter.
+            This defines how inputs will be mapped to the respective filters.
+        """
         self.keys_and_colors = keys_and_colors
         self.keys_and_index = {
             key: index for index, key in enumerate(keys_and_colors.keys())}
@@ -11,6 +31,7 @@ class FilterProjector(jft.Model):
         super().__init__(domain=sky_domain)
 
     def get_key(self, color):
+        """Returns the key that corresponds to the given color."""
         out_key = ''
         for k, v in self.keys_and_colors.items():
             if color in v:
@@ -26,11 +47,13 @@ class FilterProjector(jft.Model):
         return out_key
 
     def _get_apply(self):
+        """Returns a function that applies the projection to a given input."""
         if len(self.keys_and_index) == 1:
             key, _ = next(iter(self.keys_and_index.items()))
             return lambda x: {key: x}
         else:
-            return lambda x: {key: x[index] for key, index in self.keys_and_index.items()}
+            return lambda x: {
+                key: x[index] for key, index in self.keys_and_index.items()}
 
     def __call__(self, x):
         return self.apply(x)
