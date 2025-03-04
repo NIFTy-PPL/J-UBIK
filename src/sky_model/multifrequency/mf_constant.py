a5cfb22d430d34c4cb27ead2a92381cb88e731f0
Julian Ruestig <jruestig@posteo.de>
2025-03-04 16:03:25 +0100

    resolve: introduce some mf models

---
+++ src/sky_model/multifrequency/mf_constant.py
@@ -0,0 +1,39 @@
+from ...grid import Grid
+from ...instruments.jwst.parse.parametric_model.parametric_prior import (
+    transform_setting_to_prior_config)
+from ...instruments.jwst.parametric_model.parametric_prior import (
+    build_parametric_prior_from_prior_config)
+
+import nifty8.re as jft
+
+import jax.numpy as jnp
+
+
+class SingleValueMf(jft.Model):
+    def __init__(self, value: jft.Model, shape: tuple[int]):
+        self.value = value
+        self._shape = shape
+        super().__init__(domain=value.domain)
+
+    def __call__(self, x):
+        return jnp.full(self._shape, self.value(x))
+
+
+def build_constant_mf_from_grid(
+    grid: Grid,
+    prefix: str,
+    constant_cfg: dict,
+):
+    VALUE_KEY = 'value'
+
+    domkey = f'{prefix}_constant'
+    value_distribution = build_parametric_prior_from_prior_config(
+        domain_key=domkey,
+        prior_config=transform_setting_to_prior_config(
+            constant_cfg[VALUE_KEY]),
+        as_model=True)
+
+    # More memory efficient & performant than saving a full array.
+    shape = grid.shape[:-2] + (1, 1)
+
+    return SingleValueMf(value=value_distribution, shape=shape)
---
+++ src/sky_model/multifrequency/multifrequency_model.py
@@ -0,0 +1,32 @@
+from ...grid import Grid
+
+from .mf_model_from_grid import build_mf_model_from_grid
+from .mf_constant import build_constant_mf_from_grid
+
+NIFTY_MF = {'nifty_mf', 'niftymf'}
+CONSTANT = {'constant_mf'}
+
+SUPPORTED_MODELS = (CONSTANT | NIFTY_MF)
+
+
+def build_multifrequency_from_grid(
+    grid: Grid,
+    prefix: str,
+    model_cfg: dict,
+    **kwargs: dict
+):
+
+    assert len(model_cfg) == 1
+    model_key = next(iter(model_cfg.keys()))
+    prefix = '_'.join((prefix, model_key))
+
+    if model_key in NIFTY_MF:
+        return build_mf_model_from_grid(grid, prefix, model_cfg[model_key], **kwargs)
+
+    elif model_key in CONSTANT:
+        return build_constant_mf_from_grid(grid, prefix, model_cfg[model_key])
+
+    else:
+        raise NotImplementedError(
+            f'Invalid multifrequency model: {model_key}'
+            f'\n Supported models: {SUPPORTED_MODELS}')
