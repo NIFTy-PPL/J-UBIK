80eb1d5f48212c99f4a5af9ffde15df4a1afe76a
Julian Ruestig <jruestig@posteo.de>
2025-03-08 00:57:19 +0100

    resolve: mosaicing is in main resolve

--- src/instruments/resolve/re/__init__.py
+++ src/instruments/resolve/re/__init__.py
@@ -18,5 +18,3 @@
 from ....sky_model.resolve_sky import sky_model_diffuse, sky_model
 from ....sky_model.resolve_point_sources import resolve_point_sources
 from .response import InterferometryResponse, InterferometryResponseFinuFFT, InterferometryResponseDucc
-
-from .mosaicing import *
