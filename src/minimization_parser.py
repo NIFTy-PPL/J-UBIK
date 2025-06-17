c8d3f9b4903d976e98deec18d517dd326939ca99
Julian Ruestig <jruestig@posteo.de>
2025-06-17 18:13:29 +0200

    minimization parser: no security check in the get_range_index

--- src/minimization_parser.py
+++ src/minimization_parser.py
@@ -143,10 +143,8 @@ def get_range_index(
         if switches[i] <= iteration < switches[i+1]:
             return i
 
-    if iteration == total_iterations:
+    if iteration >= total_iterations:
         return len(switches) - 1
-    else:
-        raise ValueError(f'Iteration {iteration} is out of range.')
 
 
 def _delta_logic(
