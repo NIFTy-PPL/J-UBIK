075ffd90a9c90e3074762d2f90c2455e1def527f
Julian Ruestig <jruestig@posteo.de>
2025-06-26 15:49:29 +0200

    jwst: fixing the test

--- test/wcs/test_wcs_subsample_centers.py
+++ test/wcs/test_wcs_subsample_centers.py
@@ -1,4 +1,4 @@
-from jubik0.wcs.wcs_subsample_centers import subsample_grid_centers_in_index_grid
+from jubik0.wcs.wcs_subsample_centers import subsample_pixel_centers
 from functools import partial
 
 import numpy as np
@@ -27,26 +27,29 @@ def test_simple():
     grid = grid_setup()
 
     # Check subsample 1
-    xx, yy = grid.spatial.bounding_box_index_grid_from_world_extrema(
-        grid.spatial.world_corners(), indexing="xy"
-    )
-    xxsub, yysub = subsample_grid_centers_in_index_grid(
-        world_corners=grid.spatial.world_corners(),
-        to_be_subsampled_grid_wcs=grid.spatial,
-        index_grid_wcs=grid.spatial,
-        subsample=1,
+    xx, yy = grid.spatial.index_grid_from_bounding_indices(
+        *grid.spatial.bounding_indices_from_world_extrema(grid.spatial.world_corners()),
         indexing="xy",
+    )
+    xxsub, yysub = subsample_pixel_centers(
+        bounding_indices=grid.spatial.bounding_indices_from_world_extrema(
+            grid.spatial.world_corners()
+        ),
+        to_be_subsampled_grid_wcs=grid.spatial,
+        subsample=1,
+        as_pixel_values=True,
     )
     assert np.allclose(xx, xxsub, atol=1e-5)
     assert np.allclose(yy, yysub, atol=1e-5)
 
     # Check subsample 2
-    xxsub, yysub = subsample_grid_centers_in_index_grid(
-        world_corners=grid.spatial.world_corners(),
+    xxsub, yysub = subsample_pixel_centers(
+        bounding_indices=grid.spatial.bounding_indices_from_world_extrema(
+            grid.spatial.world_corners()
+        ),
         to_be_subsampled_grid_wcs=grid.spatial,
-        index_grid_wcs=grid.spatial,
         subsample=2,
-        indexing="xy",
+        as_pixel_values=True,
     )
     xxsub, yysub = map(partial(np.round, decimals=3), [xxsub, yysub])
     test_array = np.array(((-0.25, 0.25),) * 2)
@@ -54,12 +57,13 @@ def test_simple():
     assert np.allclose(yysub[:2, :2], test_array.T)
 
     # Check subsample 3
-    xxsub, yysub = subsample_grid_centers_in_index_grid(
-        world_corners=grid.spatial.world_corners(),
+    xxsub, yysub = subsample_pixel_centers(
+        bounding_indices=grid.spatial.bounding_indices_from_world_extrema(
+            grid.spatial.world_corners()
+        ),
         to_be_subsampled_grid_wcs=grid.spatial,
-        index_grid_wcs=grid.spatial,
         subsample=3,
-        indexing="xy",
+        as_pixel_values=True,
     )
     xxsub, yysub = map(partial(np.round, decimals=3), [xxsub, yysub])
     test_array = np.array(((-0.333, 0, 0.333),) * 3)
