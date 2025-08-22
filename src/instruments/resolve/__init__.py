c25d0da40db44e42b6aec44e4e03f16cc71f7574
Jakob Roth <roth@mpa-garching.mpg.de>
2025-08-22 16:53:01 +0200

    resolve: ms_import: don't export all functions

--- src/instruments/resolve/__init__.py
+++ src/instruments/resolve/__init__.py
@@ -1,7 +1,7 @@
 from . import data
 from .data.antenna_positions import AntennaPositions
 from .data.direction import *
-from .data.ms_import import *
+from .data.ms_import import ms2observations, ms2observations_all
 from .data.xds_import import *
 from .data.observation import *
 
