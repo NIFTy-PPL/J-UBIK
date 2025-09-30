09118d5150dd27ac6a4a7b49fc69ff923a89a617
Julian Ruestig <jruestig@posteo.de>
2025-09-30 18:15:10 +0200

    resolve: take newer version of color

--- src/color.py
+++ src/color.py
@@ -8,8 +8,6 @@
 from astropy import units as u, constants as const
 from numpy import argsort
 
-from typing import Optional
-
 
 class Color(u.Quantity):
     """A class representing color based on wavelength, frequency, or energy.
@@ -37,17 +35,16 @@ class Color(u.Quantity):
         quantity: u.Quantity,
     ):
         if not isinstance(quantity, u.Quantity):
-            raise IOError('Instantiate with a quantity that has units')
+            raise IOError("Instantiate with a quantity that has units")
 
-        if quantity.unit.physical_type == 'length':
+        if quantity.unit.physical_type == "length":
             self._init_length(quantity)
-        elif quantity.unit.physical_type == 'frequency':
+        elif quantity.unit.physical_type == "frequency":
             self._init_frequency(quantity)
-        elif quantity.unit.physical_type == 'energy':
+        elif quantity.unit.physical_type == "energy":
             self._init_energy(quantity)
         else:
-            raise ValueError(
-                f"Unsupported physical type: {quantity.physical_type}")
+            raise ValueError(f"Unsupported physical type: {quantity.physical_type}")
 
     def _init_length(self, wavelength: u.Quantity):
         # Initialization using wavelength
@@ -68,11 +65,11 @@ class Color(u.Quantity):
         self.wavelength = (const.c / self.frequency).to(u.m)
 
     def to_unit(self, unit: u.Unit):
-        if unit.physical_type == 'length':
+        if unit.physical_type == "length":
             value = self.wavelength
-        elif unit.physical_type == 'frequency':
+        elif unit.physical_type == "frequency":
             value = self.frequency
-        elif unit.physical_type == 'energy':
+        elif unit.physical_type == "energy":
             value = self.energy
         return value.to(unit)
 
@@ -89,7 +86,7 @@ class Color(u.Quantity):
         Redshifted Color: lambda(z) = lambda (1 + z).
 
         """
-        return Color((1+z)*self.wavelength)
+        return Color((1 + z) * self.wavelength)
 
 
 class ColorRange:
@@ -120,50 +117,49 @@ class ColorRange:
         self.start = start
         self.end = end
 
-    @ property
+    @property
     def center(self):
-        '''Returns the center/mean of each color range.'''
+        """Returns the center/mean of each color range."""
         return Color((self.end.energy - self.start.energy) / 2 + self.start.energy)
 
     def __contains__(self, item: Color):
         """Check if a given Color is within the range of this ColorRange."""
         assert isinstance(item, Color)
-        return (
-            (self.start.energy <= item.energy <= self.end.energy) or
-            (self.start.energy >= item.energy >= self.end.energy)
+        return (self.start.energy <= item.energy <= self.end.energy) or (
+            self.start.energy >= item.energy >= self.end.energy
         )
 
     def __repr__(self):
         """Returns a string representation of the ColorRange object."""
-        return f'ColorRange([{self.start.energy}, {self.end.energy}])'
+        return f"ColorRange([{self.start.energy}, {self.end.energy}])"
 
     def to_unit(self, unit: u.Unit) -> tuple[u.Quantity]:
         return self.start.to_unit(unit), self.end.to_unit(unit)
 
-    @ property
+    @property
     def binbounds(self):
-        '''The binbounds of the binned color range.
+        """The binbounds of the binned color range.
 
         Note
         ----
         The bins are assumed to be consecutive. Hence, the minimum of each
         color range gets returned, the maximum of the binbounds is the and the
-        maximum of the color range with the largest mean energy. '''
+        maximum of the color range with the largest mean energy."""
         return [self.start, self.end]
 
     def binbounds_in(self, unit: u.Unit):
-        '''The binbounds of the binned color ranges in the requested unit.
+        """The binbounds of the binned color ranges in the requested unit.
 
         Note
         ----
         The color bins are assumed to be consecutive in energy. Hence, the
         minimum of each color range gets returned, the maximum of the binbounds
         is the and the maximum of the color range with the largest mean energy.
-        '''
+        """
         unit_type = {
-            u.physical.length: 'wavelength',
-            u.physical.frequency: 'frequency',
-            u.physical.energy: 'energy'
+            u.physical.length: "wavelength",
+            u.physical.frequency: "frequency",
+            u.physical.energy: "energy",
         }[unit.physical_type]
 
         return [getattr(bb, unit_type).to(unit).value for bb in self.binbounds]
@@ -191,37 +187,37 @@ class ColorRanges:
         sortid = argsort([cr.center.value for cr in color_ranges])
         self.color_ranges = [color_ranges[ii] for ii in sortid]
 
-    @ property
+    @property
     def binbounds(self):
-        '''The binbounds (color) of the binned color ranges.
+        """The binbounds (color) of the binned color ranges.
 
         Note
         ----
         The bins are assumed to be consecutive. Hence, the minimum of each
         color range gets returned, the maximum of the binbounds is the and the
-        maximum of the color range with the largest mean energy. '''
+        maximum of the color range with the largest mean energy."""
         return [cr.start for cr in self.color_ranges] + [self.color_ranges[-1].end]
 
     def binbounds_in(self, unit: u.Unit):
-        '''The binbounds of the binned color ranges in the requested unit.
+        """The binbounds of the binned color ranges in the requested unit.
 
         Note
         ----
         The color bins are assumed to be consecutive in energy. Hence, the
         minimum of each color range gets returned, the maximum of the binbounds
         is the and the maximum of the color range with the largest mean energy.
-        '''
+        """
         unit_type = {
-            u.physical.length: 'wavelength',
-            u.physical.frequency: 'frequency',
-            u.physical.energy: 'energy'
+            u.physical.length: "wavelength",
+            u.physical.frequency: "frequency",
+            u.physical.energy: "energy",
         }[unit.physical_type]
 
         return [getattr(bb, unit_type).to(unit).value for bb in self.binbounds]
 
-    @ property
+    @property
     def centers(self):
-        '''Returns the centers/mean of each color range.'''
+        """Returns the centers/mean of each color range."""
         return [cr.center for cr in self.color_ranges]
 
     @property
@@ -229,17 +225,17 @@ class ColorRanges:
         return (len(self.color_ranges),)
 
     def get_color_range_from_color(self, color: Color):
-        '''Returns all color ranges which contain the color. Note: These could
-        be more then one if the color ranges are not disjoint.'''
-        assert isinstance(color, Color), f'{color} must be a Color'
-        assert color in self, f'{color} is not in {self}'
+        """Returns all color ranges which contain the color. Note: These could
+        be more then one if the color ranges are not disjoint."""
+        assert isinstance(color, Color), f"{color} must be a Color"
+        assert color in self, f"{color} is not in {self}"
         return [ii for ii, cr in enumerate(self.color_ranges) if color in cr]
 
     def __repr__(self):
-        crs = ''
+        crs = ""
         for ii, cr in enumerate(self.color_ranges):
-            crs += f'\n{ii}: {cr.__repr__()}'
-        return f'ColorRanges({crs})'
+            crs += f"\n{ii}: {cr.__repr__()}"
+        return f"ColorRanges({crs})"
 
     def __contains__(self, item: Color):
         return any([item in cr for cr in self.color_ranges])
@@ -248,11 +244,11 @@ class ColorRanges:
         return len(self.color_ranges)
 
     def __getitem__(self, index: int):
-        '''Get color range from bin index.'''
+        """Get color range from bin index."""
         return self.color_ranges[index]
 
     def to_unit(self, unit: u.Unit) -> list[tuple[u.Quantity]]:
         crs = []
-        for ii, cr in enumerate(self.color_ranges):
+        for cr in self.color_ranges:
             crs.append(cr.to_unit(unit))
         return crs
