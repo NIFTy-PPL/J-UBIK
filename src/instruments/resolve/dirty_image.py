a5714b2413d56ec75259d10a97af28f559f07f18
Julian Ruestig <jruestig@posteo.de>
2026-05-29 11:34:16 +0200

    resolve: some formatting

--- src/instruments/resolve/dirty_image.py
+++ src/instruments/resolve/dirty_image.py
@@ -15,52 +15,53 @@
 # Author: Philipp Arras, Andreas Popp
 
 from typing import Union
-from jax import linear_transpose
 
-from .data import Observation
-from .response import interferometry_response
-from .parse import Ducc0Settings, FinufftSettings
+import astropy.units as u
+import jax.numpy as jnp
+import numpy as np
+from astropy.units import Unit
+from jax import linear_transpose
+from numpy.typing import NDArray
+
 from ...grid import Grid
 from .constants import RESOLVE_SKY_UNIT, RESOLVE_SPATIAL_UNIT
-
-import jax.numpy as jnp
-from astropy.units import Unit
-import astropy.units as u
-import numpy as np
-from numpy.typing import NDArray
+from .data import Observation
+from .parse import Ducc0Settings, FinufftSettings
+from .response import interferometry_response
 
 
 def dirty_image(
-        observation: Observation,
-        sky_grid: Grid,
-        backend_settings: Union[Ducc0Settings, FinufftSettings],
-        weighting: str = "natural",
-        flux_unit: Unit = RESOLVE_SKY_UNIT,
-        ) -> u.Quantity:
+    observation: Observation,
+    sky_grid: Grid,
+    backend_settings: Union[Ducc0Settings, FinufftSettings],
+    weighting: str = "natural",
+    flux_unit: Unit = RESOLVE_SKY_UNIT,
+) -> u.Quantity:
 
     # TODO: Extract shape fully from grid
     full_sky_shape = (1, 1, 1) + tuple(sky_grid.spatial.shape)
-    
+
     R = interferometry_response(
-        observation = observation,
-        sky_grid = sky_grid,
-        backend_settings = backend_settings,
+        observation=observation,
+        sky_grid=sky_grid,
+        backend_settings=backend_settings,
     )
 
-    R_adjoint = linear_transpose(R,jnp.ones(full_sky_shape, observation.weight_val.dtype))
+    R_adjoint = linear_transpose(
+        R, jnp.ones(full_sky_shape, observation.weight_val.dtype)
+    )
 
-    
     d = observation.vis_val
     vol = sky_grid.spatial.dvol.value
 
     if weighting == "natural":
         w = observation.weight_val
-        
+
     elif weighting == "uniform":
         w = uniform_weights(
-            observation = observation,
-            sky_grid = sky_grid,
-            )    
+            observation=observation,
+            sky_grid=sky_grid,
+        )
     else:
         raise ValueError("Either 'natural' or 'uniform' weighting can be chosen.")
 
@@ -68,36 +69,33 @@ def dirty_image(
     res = R_adjoint(jnp.array(primals))[0] / vol**2 * RESOLVE_SKY_UNIT
     return res.to(flux_unit)
 
-def uniform_weights(
-        observation: Observation,
-        sky_grid: Grid
-        ) -> NDArray:
+
+def uniform_weights(observation: Observation, sky_grid: Grid) -> NDArray:
 
     weights = np.empty_like(observation.weight_val)
     u, v = observation.effective_uvw()[0:2]
 
     for ipol in range(observation.npol):
         Hnorm, xedges0, yedges0 = uvw_density(u, v, sky_grid, None)
-        H    , xedges , yedges  = uvw_density(u, v, sky_grid, observation.weight_val[ipol])
+        H, xedges, yedges = uvw_density(u, v, sky_grid, observation.weight_val[ipol])
         assert np.all(xedges == xedges0)
         assert np.all(yedges == yedges0)
         xindices = np.searchsorted(xedges, u.ravel())
         yindices = np.searchsorted(yedges, v.ravel())
-        norm = Hnorm[xindices-1, yindices-1].reshape(weights.shape[1:])
+        norm = Hnorm[xindices - 1, yindices - 1].reshape(weights.shape[1:])
         norm *= norm  # FIXME Why
-        weights[ipol] = H[xindices-1, yindices-1].reshape(weights.shape[1:]) / norm
+        weights[ipol] = H[xindices - 1, yindices - 1].reshape(weights.shape[1:]) / norm
 
         return jnp.array(weights)
 
 
-
 def uvw_density(
-        eff_u: NDArray, 
-        eff_v: NDArray,
-        sky_grid: Grid,
-        weights: NDArray,
-        ) -> Union[NDArray, NDArray, NDArray]:
-    
+    eff_u: NDArray,
+    eff_v: NDArray,
+    sky_grid: Grid,
+    weights: NDArray,
+) -> Union[NDArray, NDArray, NDArray]:
+
     if weights is not None:
         assert weights.shape == eff_u.shape
         weights = weights.ravel()
@@ -110,7 +108,12 @@ def uvw_density(
     ku = np.sort(np.fft.fftfreq(nx, dx))
     kv = np.sort(np.fft.fftfreq(ny, dy))
 
-    if np.min(u) < ku[0] or np.max(u)  >= ku[-1] or np.min(v) < kv[0] or np.max(v) >= kv[-1]:
+    if (
+        np.min(u) < ku[0]
+        or np.max(u) >= ku[-1]
+        or np.min(v) < kv[0]
+        or np.max(v) >= kv[-1]
+    ):
         raise ValueError
     H, xedges, yedges = np.histogram2d(u, v, bins=[ku, kv], weights=weights)
     return H, xedges, yedges
