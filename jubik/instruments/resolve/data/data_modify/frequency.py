resolve: fix bug in averaging.

jubik/instruments/resolve/data/data_modify/frequency.py
@@ -14,10 +14,18 @@ def freq_average_by_bins(obs: Observation, n_freq_chuncks: int | None):
     if n_freq_chuncks is None:
         return obs
 
+    if n_freq_chuncks < 1:
+        raise ValueError("The number of frequency chunks must be positive.")
+    if n_freq_chuncks > obs.nfreq:
+        raise ValueError(
+            "The number of frequency chunks cannot exceed the number of channels."
+        )
+
     logger.info(f"Frequency averaging observation to {n_freq_chuncks}")
 
-    splitted_freqs = np.array_split(obs.freq, n_freq_chuncks)
-    return freq_average_by_fmin_fmax(obs, splitted_freqs)
+    frequency_indices = np.array_split(np.arange(obs.nfreq), n_freq_chuncks)
+    splitted_obs = [get_freqs_by_slice(obs, ind) for ind in frequency_indices]
+    return _average_frequency_groups(obs, splitted_obs)
 
 
 def freq_average_by_fdom_and_n_freq_chunks(
@@ -44,31 +52,27 @@ def freq_average_by_fdom_and_n_freq_chunks(
 
     fmin_fmax_array = get_2d_binbounds(sky_frequencies, RESOLVE_SPECTRAL_UNIT)
 
-    splitted_freqs = []
+    splitted_obs = []
     n_obs_in_sky = 0
     for ff in fmin_fmax_array:
-        ofreq = restrict_by_freq(obs, ff[0], ff[-1], with_index=False).freq
+        obs_in_sky = restrict_by_freq(obs, ff[0], ff[-1], with_index=False)
 
-        if ofreq.size == 0:
+        if obs_in_sky.nfreq == 0:
             continue
         else:
             n_obs_in_sky += 1
 
-        if len(ofreq) > 2 * n_freq_chuncks:
-            tmp_splits = np.array_split(ofreq, n_freq_chuncks)
-            for tmp in tmp_splits:
-                assert tmp[0] != tmp[-1], (
-                    "Frequency chunking of data not compatible with sky frequencies."
-                )
-                splitted_freqs.append(np.array([tmp[0], tmp[-1]]))
-
-        else:
-            assert ofreq[0] != ofreq[-1], (
-                "Frequency chunking of data not compatible with sky frequencies."
+        if obs_in_sky.nfreq > 2 * n_freq_chuncks:
+            frequency_indices = np.array_split(
+                np.arange(obs_in_sky.nfreq), n_freq_chuncks
             )
-            splitted_freqs.append(np.array([ofreq[0], ofreq[-1]]))
+            splitted_obs.extend(
+                get_freqs_by_slice(obs_in_sky, ind) for ind in frequency_indices
+            )
+        else:
+            splitted_obs.append(obs_in_sky)
 
-    obs_out = freq_average_by_fmin_fmax(obs, splitted_freqs)
+    obs_out = _average_frequency_groups(obs, splitted_obs)
 
     freq_len = obs_out.freq.shape[0]
     if freq_len % n_obs_in_sky == 0:
@@ -94,6 +98,15 @@ def freq_average_by_fmin_fmax(
     for ff in fmin_fmax_array:
         splitted_obs.append(restrict_by_freq(obs, ff[0], ff[-1]))
 
+    return _average_frequency_groups(obs, splitted_obs)
+
+
+def _average_frequency_groups(
+    obs: Observation, splitted_obs: list[Observation]
+) -> Observation:
+    if len(splitted_obs) == 0:
+        raise ValueError("Cannot average an observation without frequency channels.")
+
     obs_avg = []
     for obsi in splitted_obs:
         new_vis = np.mean(obsi.vis.asnumpy(), axis=2, keepdims=True)
@@ -116,9 +129,9 @@ def freq_average_by_fmin_fmax(
     new_vis_shape = (obs.vis.shape[0], obs.vis.shape[1], len(new_freq))
     new_vis = np.zeros(new_vis_shape, obs.vis.dtype)
     new_weight = np.zeros(new_vis_shape, obs.weight.dtype)
-    for ii, obs in enumerate(obs_avg):
-        new_vis[:, :, ii] = obs.vis.asnumpy()[:, :, 0]
-        new_weight[:, :, ii] = obs.weight.asnumpy()[:, :, 0]
+    for ii, averaged_obs in enumerate(obs_avg):
+        new_vis[:, :, ii] = averaged_obs.vis.asnumpy()[:, :, 0]
+        new_weight[:, :, ii] = averaged_obs.weight.asnumpy()[:, :, 0]
 
     obs_averaged = Observation(
         obs.antenna_positions,

test/instruments/resolve/test_frequency.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pytest
-from numpy.testing import assert_array_equal
+from astropy import units as u
+from numpy.testing import assert_allclose, assert_array_equal
 
 import jubik as ju
 from jubik.instruments.resolve.data.data_modify.frequency import (
     exclude_frequency_ranges,
+    freq_average_by_bins,
+    freq_average_by_fdom_and_n_freq_chunks,
 )
 from jubik.instruments.resolve.parse.data.data_modify.frequency import (
     SpectralModify,
@@ -20,6 +23,60 @@ FREQS = np.array([1.0e9, 1.1e9, 1.2e9, 1.3e9, 1.4e9, 1.5e9])
 def build_obs(freqs=FREQS):
     pol_type = ju.polarization.PolarizationType(("LL", "RR"))
     return generate_random_obs(freqs, 20, [-1e2, 1e2], [-5, 5], pol_type)
+
+
+def assert_frequency_groups_are_averaged(obs, averaged, groups):
+    expected_freq = np.array([np.mean(obs.freq[ind]) for ind in groups])
+    expected_vis = np.stack(
+        [np.mean(obs.vis_val[..., ind], axis=2) for ind in groups], axis=2
+    )
+    expected_weight = np.stack(
+        [
+            len(ind) ** 2 / np.sum(1 / obs.weight_val[..., ind], axis=2)
+            for ind in groups
+        ],
+        axis=2,
+    )
+
+    assert_allclose(averaged.freq, expected_freq)
+    assert_allclose(averaged.vis_val, expected_vis)
+    assert_allclose(averaged.weight_val, expected_weight)
+
+
+@pmp("n_freq,n_bins", ((4, 2), (5, 2), (6, 2), (4, 1), (4, 4)))
+def test_freq_average_by_bins_uses_every_channel(n_freq, n_bins):
+    np.random.seed(40 + n_freq + n_bins)
+    freqs = 1.0e9 + np.arange(n_freq) * 0.1e9
+    obs = build_obs(freqs)
+
+    averaged = freq_average_by_bins(obs, n_bins)
+    groups = np.array_split(np.arange(n_freq), n_bins)
+
+    assert_frequency_groups_are_averaged(obs, averaged, groups)
+
+
+def test_freq_average_by_bins_none_is_noop():
+    obs = build_obs()
+    assert freq_average_by_bins(obs, None) is obs
+
+
+@pmp("n_bins", (0, -1, len(FREQS) + 1))
+def test_freq_average_by_bins_rejects_invalid_number_of_bins(n_bins):
+    obs = build_obs()
+    with pytest.raises(ValueError):
+        freq_average_by_bins(obs, n_bins)
+
+
+def test_frequency_domain_averaging_uses_every_channel_in_each_chunk():
+    np.random.seed(48)
+    freqs = 1.0e9 + np.arange(6) * 0.1e9
+    obs = build_obs(freqs)
+    sky_frequencies = ju.Color([0.95e9, 1.55e9] * u.Hz)
+
+    averaged = freq_average_by_fdom_and_n_freq_chunks(sky_frequencies, obs, 2)
+    groups = np.array_split(np.arange(obs.nfreq), 2)
+
+    assert_frequency_groups_are_averaged(obs, averaged, groups)
 
 
 def test_exclude_range_drops_channels_and_keeps_rest_bitwise():