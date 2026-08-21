09484d9194442381723ffbe78771dfd432f83d7c
Andreas Popp <Popp.Andreas@campus.lmu.de>
2026-08-21 09:46:21 +0200

    RESOLVE: Added file for unit and axis conventions in the scope of the resolve instrument

---
+++ jubik/instruments/resolve/conventions.py
@@ -0,0 +1,6 @@
+import astropy.units as u
+
+# Expected units for the radiointerferometric response implementations
+RESPONSE_SKY_UNIT = u.Jy / u.sr
+RESPONSE_SPATIAL_UNIT = u.rad
+RESPONSE_SPECTRAL_UNIT = u.Hz
